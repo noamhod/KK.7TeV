88f51a0a495e2a6f6005d205337c658828e2ce4e
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-07-26 22:01:34 +0000

    rearrange

---
+++ tags/z0analysis-1.0.3/include/kinematics.h
@@ -0,0 +1,43 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+#include "basicIncludes.h"
+
+#ifndef KINEMATICS_H
+#define KINEMATICS_H
+
+class kinematics
+{
+	public:
+		// utilities
+		TLorentzVector m_pTmp;
+
+	public:
+		kinematics();
+		~kinematics();
+	
+		void kinitialize();
+                void kfinalize();
+	
+		double pT(   TLorentzVector* p );
+		double eta(  TLorentzVector* p );
+		double pAbs( TLorentzVector* p );
+		double y(    TLorentzVector* p );
+
+		double dimuonCosth(          TLorentzVector* pa, TLorentzVector* pb );
+		double dR(                   TLorentzVector* pa, TLorentzVector* pb );
+		double imass(                TLorentzVector* pa, TLorentzVector* pb );
+		double QT(                   TLorentzVector* pa, TLorentzVector* pb );
+		double ySystem(              TLorentzVector* pa, TLorentzVector* pb );
+		double cosThetaCollinsSoper( TLorentzVector* pa, double ca,
+					     TLorentzVector* pb, double cb );
+
+	private:
+
+};
+#endif
+
---
+++ tags/z0analysis-1.0.3/include/muon_muid.h
@@ -0,0 +1,29 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+#include "basicIncludes.h"
+
+#ifndef MUON_MUID_H
+#define MUON_MUID_H
+
+class muon_muid : virtual public particleBase
+{
+	public:
+
+	public:
+		muon_muid();
+		muon_muid(physics* phys);
+		~muon_muid();
+
+		virtual int  getNParticles();
+		virtual void setParticle(int index); // mandatory to implement
+		virtual void turnonBraches();
+			void setMore(int index);     // user input, not mandatory
+
+};
+#endif
+

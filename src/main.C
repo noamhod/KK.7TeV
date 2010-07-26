4585874455a295e14f8ca9e5b5ff354e190101b8
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-07-26 20:19:42 +0000

    initial import

---
+++ src/main.C
@@ -0,0 +1,20 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 26/07/2010 12:36 */
+/* * * * * * * * * * * */
+
+void compileAll(TString sGRLtag)
+{
+	gROOT->ProcessLine(".include GoodRunsLists-" + sGRLtag + "/");
+	gROOT->ProcessLine(".include GoodRunsLists-" + sGRLtag + "/GoodRunsLists/");
+	gROOT->ProcessLine(".L GoodRunsLists-" + sGRLtag + "/StandAlone/libGoodRunsLists.so");
+	gROOT->ProcessLine(".L analysisControl.C++");
+	gROOT->ProcessLine("analysisControl ac");
+}
+
+void loop(Long64_t startEvent=0, Long64_t stopEvent=0)
+{
+	ac.loop(startEvent, stopEvent);
+}

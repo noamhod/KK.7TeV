518bfd9b32b0423b37b5b8a8fa446cbe9421a966
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-07-27 09:31:30 +0000

    redo structure

--- tags/z0analysis-1.1.0/main.C
+++ tags/z0analysis-1.1.0/main.C
@@ -5,25 +5,35 @@
 /* on 26/07/2010 12:36 */
 /* * * * * * * * * * * */
 
-void compileAll(TString sGRLtag)
+#define analysisControl_cxx
+#include "src/analysisControl.C"
+
+void include(TString sGRLtag)
 {
-	// add this software include paths
-	gROOT->ProcessLine(".include include/");
-	gROOT->ProcessLine(".include src/");
+	gROOT->ProcessLine(".include .");
+        gROOT->ProcessLine(".include include/");
+        gROOT->ProcessLine(".include src/");
 
-	// add the GRL include paths
-	gROOT->ProcessLine(".include GoodRunsLists-" + sGRLtag + "/");
-	gROOT->ProcessLine(".include GoodRunsLists-" + sGRLtag + "/GoodRunsLists/");
+        gROOT->ProcessLine(".include GoodRunsLists-" + sGRLtag + "/");
+        gROOT->ProcessLine(".include GoodRunsLists-" + sGRLtag + "/GoodRunsLists/");
+}
 
-	// load the GRL lib
+void loadGRL(TString sGRLtag)
+{
+	include(sGRLtag);
 	gROOT->ProcessLine(".L GoodRunsLists-" + sGRLtag + "/StandAlone/libGoodRunsLists.so");
+}
 
-	// compile the analysisControl code and create an instance
-	gROOT->ProcessLine(".L src/analysisControl.C++");
+void compile(TString sGRLtag)
+{
+	loadGRL(sGRLtag);
+        gROOT->ProcessLine(".L src/analysisControl.C++");
+}
+
+void loadCompiled(TString sGRLtag)
+{
+	loadGRL(sGRLtag);
+	gROOT->ProcessLine(".L src/analysisControl_C.so");
 	gROOT->ProcessLine("analysisControl ac");
 }
 
-void loop(Long64_t startEvent=0, Long64_t stopEvent=0)
-{
-	ac.loop(startEvent, stopEvent);
-}

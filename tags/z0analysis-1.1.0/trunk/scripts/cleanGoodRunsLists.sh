7ed9cea5587a184351f1641b8faa097b3a022b36
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-07-27 09:54:04 +0000

    restructure

---
+++ tags/z0analysis-1.1.0/trunk/scripts/cleanGoodRunsLists.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+cd ../GoodRunsLists-00-00-84/
+rm -rf StandAlone/libGoodRunsLists.so
+rm -rf Root/*.o
+rm -rf .svn/ test/.svn/ src/.svn/ python/.svn/ scripts/.svn/ run/.svn/ GoodRunsLists/.svn/ cmt/.svn/ Root/.svn/ src/components/.svn/ run/lumi_files/.svn/
+cd -
+

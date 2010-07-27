f2a52e33f255f44348a75e592300e1ad19d80d04
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-07-27 10:06:40 +0000

    fix some inconsistencies

--- trunk/scripts/makeTarball.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/
-name=`date +Day%d%m%YTime%H%M`
-suffix='*.h *.C *.sh *.root *.list *.cuts *.inp *.xml'
-mkdir $HOME/d3pdAnalysis/$name
-
-#cp -rf $suffix $HOME/d3pdAnalysis/$name/
-cp -rf * $HOME/d3pdAnalysis/$name/

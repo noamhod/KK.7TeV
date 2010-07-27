7b1561fdc20beac82f2d9688442ed645798cb8c8
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-07-27 22:13:34 +0000

    remove hard coded paths

--- src/utilities.C
+++ src/utilities.C
@@ -28,6 +28,100 @@ void utilities::ufinalize()
 
 }
 
+string utilities::checkANDsetFilepath(string envPath, string fileName)
+{
+        char * tmpPath = getenv(envPath.c_str());
+        if (!tmpPath) {
+                cerr << "environment variable " << envPath  <<  " not defined" << endl;
+                cerr << "\tdid you forget to 'source configure_bash or configure_tcsh' ?" << endl;
+                exit(-1);
+        }
+
+        string fullPath = tmpPath;
+        fullPath += fileName;
+
+        //----------------- check if there are ".."-like parts in the fullPath
+        int i=0;
+        int pos=0;
+        bool isDoubledot = false;
+        while (i<(int)fullPath.size()-1) { // check if there's a ".." string in fullPath
+                if (fullPath.substr(i,2) == "..") {
+                        pos = i;
+                        isDoubledot = true;
+                        //cout << "warning: this is a known problem with file paths, fixing it now..." << endl;
+                }
+                i++;
+        }
+
+        if (isDoubledot) {
+                string oldPath = fullPath;
+                string fin = fullPath.substr(pos+2,(int)fullPath.size()-(pos+2)); // remember everything from after ".."
+                string newpath = fullPath.substr(0,pos-1); // remember everything before "/.."
+                int newpos = pos-2;
+                string isBackslash = newpath.substr(newpos,1);
+                while (isBackslash != "/") { // remove the dir name before "/.."
+                        newpos--;
+                        isBackslash = newpath.substr(newpos,1);
+                }
+                fullPath = newpath.substr(0,newpos) + fin;
+                //cout << "\t file path was: " << oldPath << endl;
+                //cout << "\t corrected to: " << fullPath << endl;
+                cout << "file path: " << fullPath << endl;
+        }
+
+        if (!isDoubledot) {cout << "file path: " << fullPath << endl;}
+
+        return(fullPath);
+}
+
+string utilities::removeDoubleDot(string spath)
+{
+	//----------------- check if there are ".."-like parts in the fullPath
+        int i=0;
+        int pos=0;
+        bool isDoubledot = false;
+        while (i<(int)spath.size()-1)
+	{ // check if there's a ".." string in fullPath
+                if (spath.substr(i,2) == "..")
+		{
+                        pos = i;
+                        isDoubledot = true;
+                }
+                i++;
+        }
+	
+	if (isDoubledot)
+	{
+                string oldPath = spath;
+                string fin = spath.substr(pos+2,(int)spath.size()-(pos+2)); // remember everything from after ".."
+                string newpath = spath.substr(0,pos-1); // remember everything before "/.."
+                int newpos = pos-2;
+                string isBackslash = newpath.substr(newpos,1);
+                while (isBackslash != "/") { // remove the dir name before "/.."
+                        newpos--;
+                        isBackslash = newpath.substr(newpos,1);
+                }
+                spath = newpath.substr(0,newpos) + fin;
+        }
+
+	return spath;
+}
+
+// a method to chaeck is the environment veriable is defined or not
+// will NOT exit if the environment variable is not defined.
+bool utilities::checkPath(string envPath)
+{
+        bool isDefined = false;
+        char * tmpPath = getenv(envPath.c_str());
+        if (!tmpPath) {
+                cerr << "environment variable " << envPath  <<  " not defined" << endl;
+                cerr << "\tdid you forget to 'source configure_bash or configure_tcsh' ?" << endl;
+                isDefined = false;
+        }
+        else {isDefined = true;}
+        return(isDefined);
+}
+
 void utilities::parseKeyValLine(string sLine)
 {
 

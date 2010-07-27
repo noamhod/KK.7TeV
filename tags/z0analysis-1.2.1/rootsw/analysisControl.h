7e1c8a67c17937c9cd53d69cd0fcbf6092f7c89a
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-07-27 12:00:43 +0000

    move GRL

---
+++ tags/z0analysis-1.2.1/rootsw/analysisControl.h
@@ -0,0 +1,76 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:23 */
+/* * * * * * * * * * * */
+
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:22 */
+/* * * * * * * * * * * */
+
+#include "basicIncludes.h"
+
+#define chains_cxx
+#include "chains.C"
+
+#define analysis_cxx
+#include "analysis.C"
+
+#ifndef ANALYSISCONTROL_H
+#define ANALYSISCONTROL_H
+
+class analysisControl : public chains, public analysis
+{
+	public:
+		// from MakeClass
+		physics* m_phys;		
+
+		// pointers
+		TFile*          m_histfile;
+		TFile*          m_treefile;
+		
+		TDirectory*     m_dirAllCuts;
+		TDirectory* 	m_dirNoCuts;
+		TDirectory* 	m_dirCutFlow;
+		
+		analysis*       m_analysis;
+		graphicObjects* m_graphics;
+		GRLinterface*   m_GRL; 
+
+		// run control
+		Long64_t l64t_nentries;
+		Long64_t l64t_nbytes;
+		Long64_t l64t_nb;
+		Long64_t l64t_jentry;
+		Long64_t l64t_ientry;
+	
+		Long64_t l64t_mod;
+		Long64_t l64t_startEvent;
+		Long64_t l64t_stopEvent;
+	
+	public:
+		analysisControl();
+		~analysisControl();
+		void   initialize();
+		void   finalize();
+		void   book();
+		void   draw();
+		void   analyze();
+		void   makeTreeFriends();
+		void   loop(Long64_t startEvent = 0, Long64_t stopAfterNevents = kMaxLong64);
+
+	private:
+};
+#endif
+

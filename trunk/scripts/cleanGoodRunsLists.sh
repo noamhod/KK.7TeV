e1f18d6f0b788a336a3f13bbfeab5cc42f9ff991
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-07-27 10:14:20 +0000

    test

---
+++ trunk/scripts/cleanGoodRunsLists.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+cd ../GoodRunsLists-00-00-84/
+rm -f StandAlone/libGoodRunsLists.so
+rm -f Root/*.o
+rm -f .svn/ test/.svn/ src/.svn/ python/.svn/ scripts/.svn/ run/.svn/ GoodRunsLists/.svn/ cmt/.svn/ Root/.svn/ src/components/.svn/ run/lumi_files/.svn/
+cd -
+

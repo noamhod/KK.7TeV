ab0cb04105a66264dad0d06a7b65f9f42e0f5860
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-08-01 12:46:36 +0000

    changed tree strategy

--- trunk/include/analysis.h
+++ trunk/include/analysis.h
@@ -19,11 +19,11 @@
 #define muon_staco_cxx
 #include "muon_staco.C"
 
-#define muon_staco_tree_cxx
-#include "muon_staco_tree.C"
-
 #define muon_muid_cxx
 #include "muon_muid.C"
+
+#define offlineTree_cxx
+#include "offlineTree.C"
 
 #ifndef ANALYSIS_H
 #define ANALYSIS_H
@@ -39,41 +39,19 @@ class analysis : public physics, public selection, public graphicObjects
 		muon_muid*      m_muid;
 		muon_staco*     m_mustaco;
 		
-		muon_staco_tree* m_mustacotree;
-
 		GRLinterface*   m_analysis_grl;
 
+		offlineTree*    m_offlineTree;
 
 		// variables
 		TMapsd*         m_cutFlowMap;
 		TMapds*         m_cutFlowOrdered;
 		TMapsi*         m_cutFlowNumbers;
-
+		
 		TFile*		m_treeFile;
-		TTree*		m_eventLevelTree;
-		TDirectory*	m_dirEventLevel;
-		TDirectory*	m_dir_muon_staco;
 
-		// branches for the trees
-                vector<double>* vd_imass;
-                vector<double>* vd_costh;
-                vector<double>* vd_sum_pTid;
-		int    i_runnumber;
-                int    i_lumiblock;
-		Bool_t b_isgrl;
-		Bool_t b_l1mu6;
-
-		// event level variables
-		double d_imass;
-		double d_costh;
-		double d_sum_pTid;
-		Bool_t b_isL1MU6;
-		int    i_run;
-                int    i_lum;
 		Bool_t b_isGRL;
-
-		// other
-		int nAllEvents;
+		int    nAllEvents;
 
 	public:
 		analysis();
@@ -83,28 +61,19 @@ class analysis : public physics, public selection, public graphicObjects
 		void initialize();
 		void finalize();
 
-		void setTreeBranches(TFile* treeFile, string sname);
-
 		void enableGeneralBranches();
 
 		void printCutFlowNumbers(Long64_t chainEntries);
 		void fillCutFlow(string sCurrentCutName, TMapsd& values2fill);
 
-		void executeBasic(bool isendofrun);
-		void executeAdvanced(bool isendofrun);
+		void executeTree(bool isendofrun);
+		void executeBasic();
+		void executeAdvanced();
 		void executeCutFlow();
 
 		void    readCutFlow(string sCutFlowFilePath);
 		TMapsd* getCutFlowMapPtr();
 		TMapds* getCutFlowOrderedPtr();
-
-		// for the tree:
-		void setEventLevelTree(string sname, TDirectory* dir);
-		void setEventLevelBranches();
-		void clearEventLevelVectors();
-		void fillEventLevelVectors();
-		void fillEventLevelTree();
-		void writeEventLevelTree();
 
 	private:
 

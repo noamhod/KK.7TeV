1acb3b764e3d6f9e139fcd1ca5bfecabb3b6e65c
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-08-17 14:09:52 +0000

    fix panda script

--- trunk/run/panda_run.sh
+++ trunk/run/panda_run.sh
@@ -6,15 +6,21 @@ dateandhour=`date +d%d%m%Y.h%H%M`
 # remove the arlier outputs
 rm -f input.txt
 rm -f z0analysis.root
+rm -f libGoodRunsLists.so
+
+../GoodRunsLists-00-00-84/StandAlone/
 
 # get the latest grl and cut flow
 cp -f ../conf/cutFlow.cuts .
 cp -f ../conf/Z_GRL_152844-159224.xml .
+
+# get the GRL lib
+cp -f ../GoodRunsLists-00-00-84/StandAlone/libGoodRunsLists.so .
 
 # submit the panda run
 #prun --exec "echo %IN > input.txt; root.exe analysisGridControlRun.C;" --athenaTag=15.6.9 --outDS user.hod.z0analysis.WZphys.Aug17 --outputs z0analysis.root --inDS group10.phys-sm.data10_7TeV.0015*.physics_MuonswBeam.*.WZphys.*.D3PD  --nFiles 10
 
 #prun --exec "echo %IN > input.txt; root.exe analysisGridControlRun.C;" --athenaTag=15.6.9 --outDS user.hod.z0analysis.WZphys.Aug17.time1246 --outputs z0analysis.root --inDS group10.phys-sm.data10_7TeV.00159224.physics_MuonswBeam.recon.ESD.x30.WZphys.100612.02.D3PD  --nFiles 10 --extFile analysisGridControl_C.so
 
-prun --exec "echo %IN > input.txt;  root.exe analysisGridControlRun.C;" --athenaTag=15.6.9 --outDS user.hod.z0analysis.$dateandhour --outputs z0analysis.root --inDS group10.phys-sm.data10_7TeV.00159224.physics_MuonswBeam.recon.ESD.x30.WZphys.100612.02.D3PD  --nFiles 10 --extFile analysisGridControl_C.so
+prun --exec "echo %IN > input.txt;  root.exe analysisGridControlRun.C;" --athenaTag=15.6.9 --outDS user.hod.z0analysis.$dateandhour --outputs z0analysis.root --inDS group10.phys-sm.data10_7TeV.00159224.physics_MuonswBeam.recon.ESD.x30.WZphys.100612.02.D3PD  --nFiles 10 --extFile analysisGridControl_C.so libGoodRunsLists.so
 

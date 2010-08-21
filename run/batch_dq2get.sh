0d41ffd7b8459f56dd04a2cf725a7265785398c3
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-08-21 00:14:13 +0000

    bug fixed

--- run/batch_dq2get.sh
+++ run/batch_dq2get.sh
@@ -5,185 +5,185 @@
 # script        |  number | set |         |        |
 #---------------------------------------------------
 
-source dq2get.sh   152844   01   19082010   2259
-source dq2get.sh   152844   02   19082010   2300
-source dq2get.sh   152844   03   19082010   2301
+source dq2get_dataset.sh   152844   01   19082010   2259
+source dq2get_dataset.sh   152844   02   19082010   2300
+source dq2get_dataset.sh   152844   03   19082010   2301
 
-source dq2get.sh   152845   01   19082010   2302
-source dq2get.sh   152845   02   19082010   2303
-source dq2get.sh   152845   03   19082010   2304
+source dq2get_dataset.sh   152845   01   19082010   2302
+source dq2get_dataset.sh   152845   02   19082010   2303
+source dq2get_dataset.sh   152845   03   19082010   2304
 
-source dq2get.sh   152878   01   19082010   2305
-source dq2get.sh   152878   02   19082010   2306
-source dq2get.sh   152878   03   19082010   2306
-source dq2get.sh   152878   04   19082010   2307
+source dq2get_dataset.sh   152878   01   19082010   2305
+source dq2get_dataset.sh   152878   02   19082010   2306
+source dq2get_dataset.sh   152878   03   19082010   2306
+source dq2get_dataset.sh   152878   04   19082010   2307
 
-source dq2get.sh   152933   01   19082010   2308
-source dq2get.sh   152933   02   19082010   2309
-source dq2get.sh   152933   03   19082010   2310
+source dq2get_dataset.sh   152933   01   19082010   2308
+source dq2get_dataset.sh   152933   02   19082010   2309
+source dq2get_dataset.sh   152933   03   19082010   2310
 
-source dq2get.sh   152994   01   19082010   2311
-source dq2get.sh   152994   02   19082010   2312
-source dq2get.sh   152994   03   19082010   2312
-source dq2get.sh   152994   04   19082010   2313
+source dq2get_dataset.sh   152994   01   19082010   2311
+source dq2get_dataset.sh   152994   02   19082010   2312
+source dq2get_dataset.sh   152994   03   19082010   2312
+source dq2get_dataset.sh   152994   04   19082010   2313
 
-source dq2get.sh   153030   01   19082010   2314
-source dq2get.sh   153030   02   19082010   2315
-source dq2get.sh   153030   03   19082010   2315
+source dq2get_dataset.sh   153030   01   19082010   2314
+source dq2get_dataset.sh   153030   02   19082010   2315
+source dq2get_dataset.sh   153030   03   19082010   2315
 
-source dq2get.sh   153134   01   19082010   2316
-source dq2get.sh   153134   02   19082010   2317
-source dq2get.sh   153134   03   19082010   2318
-source dq2get.sh   153134   04   19082010   2318
-source dq2get.sh   153134   05   19082010   2319
+source dq2get_dataset.sh   153134   01   19082010   2316
+source dq2get_dataset.sh   153134   02   19082010   2317
+source dq2get_dataset.sh   153134   03   19082010   2318
+source dq2get_dataset.sh   153134   04   19082010   2318
+source dq2get_dataset.sh   153134   05   19082010   2319
 
-source dq2get.sh   153136   01   19082010   2320
-source dq2get.sh   153136   02   19082010   2321
-source dq2get.sh   153136   03   19082010   2322
-source dq2get.sh   153136   04   19082010   2322
+source dq2get_dataset.sh   153136   01   19082010   2320
+source dq2get_dataset.sh   153136   02   19082010   2321
+source dq2get_dataset.sh   153136   03   19082010   2322
+source dq2get_dataset.sh   153136   04   19082010   2322
 
-source dq2get.sh   153159   01   19082010   2323
-source dq2get.sh   153159   02   19082010   2324
-source dq2get.sh   153159   03   19082010   2325
-source dq2get.sh   153159   04   19082010   2326
+source dq2get_dataset.sh   153159   01   19082010   2323
+source dq2get_dataset.sh   153159   02   19082010   2324
+source dq2get_dataset.sh   153159   03   19082010   2325
+source dq2get_dataset.sh   153159   04   19082010   2326
 
-source dq2get.sh   153200   01   19082010   2327
-source dq2get.sh   153200   02   19082010   2328
-#source dq2get.sh   153200   03   19082010  #### this data set has no files 
-source dq2get.sh   153200   04   19082010   2328
-source dq2get.sh   153200   05   19082010   2329
+source dq2get_dataset.sh   153200   01   19082010   2327
+source dq2get_dataset.sh   153200   02   19082010   2328
+#source dq2get_dataset.sh   153200   03   19082010  #### this data set has no files 
+source dq2get_dataset.sh   153200   04   19082010   2328
+source dq2get_dataset.sh   153200   05   19082010   2329
 
-source dq2get.sh   153565   01   19082010   2330
-source dq2get.sh   153565   02   19082010   2331
-source dq2get.sh   153565   03   19082010   2332
+source dq2get_dataset.sh   153565   01   19082010   2330
+source dq2get_dataset.sh   153565   02   19082010   2331
+source dq2get_dataset.sh   153565   03   19082010   2332
 
-source dq2get.sh   153599   01   19082010   2333
-source dq2get.sh   153599   02   19082010   2334
-source dq2get.sh   153599   03   19082010   2335
-source dq2get.sh   153599   04   19082010   2335
+source dq2get_dataset.sh   153599   01   19082010   2333
+source dq2get_dataset.sh   153599   02   19082010   2334
+source dq2get_dataset.sh   153599   03   19082010   2335
+source dq2get_dataset.sh   153599   04   19082010   2335
 
-source dq2get.sh   154810   01   19082010   2336
-source dq2get.sh   154810   02   19082010   2337
-source dq2get.sh   154810   03   20082010   1135
-source dq2get.sh   154810   04   20082010   1136
-source dq2get.sh   154810   05   20082010   1137
-source dq2get.sh   154810   06   20082010   1138
-source dq2get.sh   154810   07   19082010   2332
-source dq2get.sh   154810   08   19082010   2343
+source dq2get_dataset.sh   154810   01   19082010   2336
+source dq2get_dataset.sh   154810   02   19082010   2337
+source dq2get_dataset.sh   154810   03   20082010   1135
+source dq2get_dataset.sh   154810   04   20082010   1136
+source dq2get_dataset.sh   154810   05   20082010   1137
+source dq2get_dataset.sh   154810   06   20082010   1138
+source dq2get_dataset.sh   154810   07   19082010   2332
+source dq2get_dataset.sh   154810   08   19082010   2343
 
-source dq2get.sh   154813   01   19082010   2344
-source dq2get.sh   154813   02   19082010   2345
-source dq2get.sh   154813   03   19082010   2345
-source dq2get.sh   154813   04   19082010   2346
+source dq2get_dataset.sh   154813   01   19082010   2344
+source dq2get_dataset.sh   154813   02   19082010   2345
+source dq2get_dataset.sh   154813   03   19082010   2345
+source dq2get_dataset.sh   154813   04   19082010   2346
 
-source dq2get.sh   154815   01   19082010   2347
-source dq2get.sh   154815   02   19082010   2348
-source dq2get.sh   154815   03   19082010   2349
-source dq2get.sh   154815   04   19082010   2350
+source dq2get_dataset.sh   154815   01   19082010   2347
+source dq2get_dataset.sh   154815   02   19082010   2348
+source dq2get_dataset.sh   154815   03   19082010   2349
+source dq2get_dataset.sh   154815   04   19082010   2350
 
-source dq2get.sh   154817   01   19082010   2351
-source dq2get.sh   154817   02   19082010   2352
-source dq2get.sh   154817   03   19082010   2353
-source dq2get.sh   154817   04   19082010   2354
+source dq2get_dataset.sh   154817   01   19082010   2351
+source dq2get_dataset.sh   154817   02   19082010   2352
+source dq2get_dataset.sh   154817   03   19082010   2353
+source dq2get_dataset.sh   154817   04   19082010   2354
 
-source dq2get.sh   154822   01   19082010   2355
-source dq2get.sh   154822   02   19082010   2356
-source dq2get.sh   154822   03   19082010   2356
-source dq2get.sh   154822   04   19082010   2358
+source dq2get_dataset.sh   154822   01   19082010   2355
+source dq2get_dataset.sh   154822   02   19082010   2356
+source dq2get_dataset.sh   154822   03   19082010   2356
+source dq2get_dataset.sh   154822   04   19082010   2358
 
-source dq2get.sh   155073   01   19082010   2358
-source dq2get.sh   155073   02   19082010   2359
-source dq2get.sh   155073   03   20082010   0000
-source dq2get.sh   155073   04   20082010   0001
+source dq2get_dataset.sh   155073   01   19082010   2358
+source dq2get_dataset.sh   155073   02   19082010   2359
+source dq2get_dataset.sh   155073   03   20082010   0000
+source dq2get_dataset.sh   155073   04   20082010   0001
 
-source dq2get.sh   155112   01   20082010   0002 
-source dq2get.sh   155112   02   20082010   0004
-source dq2get.sh   155112   03   20082010   0005
+source dq2get_dataset.sh   155112   01   20082010   0002 
+source dq2get_dataset.sh   155112   02   20082010   0004
+source dq2get_dataset.sh   155112   03   20082010   0005
 
-source dq2get.sh   155116   01   20082010   0007
-source dq2get.sh   155116   02   20082010   0008
-source dq2get.sh   155116   03   20082010   0009
+source dq2get_dataset.sh   155116   01   20082010   0007
+source dq2get_dataset.sh   155116   02   20082010   0008
+source dq2get_dataset.sh   155116   03   20082010   0009
 
-source dq2get.sh   155160   01   20082010   0010
-source dq2get.sh   155160   02   20082010   0011
-source dq2get.sh   155160   03   20082010   0012
+source dq2get_dataset.sh   155160   01   20082010   0010
+source dq2get_dataset.sh   155160   02   20082010   0011
+source dq2get_dataset.sh   155160   03   20082010   0012
 
-source dq2get.sh   155228   01   20082010   0013
-source dq2get.sh   155228   02   20082010   0014
-source dq2get.sh   155228   03   20082010   0014
+source dq2get_dataset.sh   155228   01   20082010   0013
+source dq2get_dataset.sh   155228   02   20082010   0014
+source dq2get_dataset.sh   155228   03   20082010   0014
 
-source dq2get.sh   155280   01   20082010   0015
-source dq2get.sh   155280   02   20082010   0016
-source dq2get.sh   155280   03   20082010   0017
+source dq2get_dataset.sh   155280   01   20082010   0015
+source dq2get_dataset.sh   155280   02   20082010   0016
+source dq2get_dataset.sh   155280   03   20082010   0017
 
-source dq2get.sh   155569   01   20082010   0018
-source dq2get.sh   155569   02   20082010   0019
-source dq2get.sh   155569   03   20082010   0020
+source dq2get_dataset.sh   155569   01   20082010   0018
+source dq2get_dataset.sh   155569   02   20082010   0019
+source dq2get_dataset.sh   155569   03   20082010   0020
 
-source dq2get.sh   155634   01   20082010   0021
-source dq2get.sh   155634   02   20082010   0022
-source dq2get.sh   155634   03   20082010   0022
+source dq2get_dataset.sh   155634   01   20082010   0021
+source dq2get_dataset.sh   155634   02   20082010   0022
+source dq2get_dataset.sh   155634   03   20082010   0022
 
-source dq2get.sh   155669   01   20082010   0023
-source dq2get.sh   155669   02   20082010   0024
-source dq2get.sh   155669   03   20082010   0025
+source dq2get_dataset.sh   155669   01   20082010   0023
+source dq2get_dataset.sh   155669   02   20082010   0024
+source dq2get_dataset.sh   155669   03   20082010   0025
 
-source dq2get.sh   155678   01   20082010   0026
-source dq2get.sh   155678   02   20082010   0027
-source dq2get.sh   155678   03   20082010   0028
+source dq2get_dataset.sh   155678   01   20082010   0026
+source dq2get_dataset.sh   155678   02   20082010   0027
+source dq2get_dataset.sh   155678   03   20082010   0028
 
-source dq2get.sh   155697   01   20082010   0029
-source dq2get.sh   155697   02   20082010   0031
-source dq2get.sh   155697   03   20082010   0032
+source dq2get_dataset.sh   155697   01   20082010   0029
+source dq2get_dataset.sh   155697   02   20082010   0031
+source dq2get_dataset.sh   155697   03   20082010   0032
 
-source dq2get.sh   156682   01   20082010   0033
+source dq2get_dataset.sh   156682   01   20082010   0033
 
-source dq2get.sh   158045   01   20082010   0034
+source dq2get_dataset.sh   158045   01   20082010   0034
 
-source dq2get.sh   158116   01   20082010   0036
-source dq2get.sh   158116   02   20082010   0038
+source dq2get_dataset.sh   158116   01   20082010   0036
+source dq2get_dataset.sh   158116   02   20082010   0038
 
-source dq2get.sh   158269   01   20082010   0039
+source dq2get_dataset.sh   158269   01   20082010   0039
 
-source dq2get.sh   158299   01   20082010   0040
+source dq2get_dataset.sh   158299   01   20082010   0040
 
-source dq2get.sh   158392   01   20082010   0041
+source dq2get_dataset.sh   158392   01   20082010   0041
 
-source dq2get.sh   158443   01   20082010   0042
+source dq2get_dataset.sh   158443   01   20082010   0042
 
-source dq2get.sh   158466   01   20082010   0043
+source dq2get_dataset.sh   158466   01   20082010   0043
 
-source dq2get.sh   158545   01   20082010   0044
+source dq2get_dataset.sh   158545   01   20082010   0044
 
-source dq2get.sh   158548   01   20082010   0044
-#source dq2get.sh   158548   02   20082010  #### resubmitted (3 times)
+source dq2get_dataset.sh   158548   01   20082010   0044
+#source dq2get_dataset.sh   158548   02   20082010  #### resubmitted (3 times)
 
-source dq2get.sh   158582   01   20082010   0047
-#source dq2get.sh   158582   02   20082010   0049  ### 1/54 FAILED - not resubmitted yet
+source dq2get_dataset.sh   158582   01   20082010   0047
+#source dq2get_dataset.sh   158582   02   20082010   0049  ### 1/54 FAILED - not resubmitted yet
 
-source dq2get.sh   158632   01   20082010   0052
+source dq2get_dataset.sh   158632   01   20082010   0052
 
-source dq2get.sh   158801   01   20082010   0053
+source dq2get_dataset.sh   158801   01   20082010   0053
 
-source dq2get.sh   158975   01   20082010   0055
+source dq2get_dataset.sh   158975   01   20082010   0055
 
-source dq2get.sh   159041   01   20082010   0057
+source dq2get_dataset.sh   159041   01   20082010   0057
 
-source dq2get.sh   159086   01   20082010   0058
-#source dq2get.sh   159086   02   #######   #### resubmitted (3 times !)
-source dq2get.sh   159086   03   20082010   0102
+source dq2get_dataset.sh   159086   01   20082010   0058
+source dq2get_dataset.sh   159086   02   20082010   2355
+source dq2get_dataset.sh   159086   03   20082010   0102
 
-source dq2get.sh   159113   01   20082010   0104
-source dq2get.sh   159113   02   20082010   0105
+source dq2get_dataset.sh   159113   01   20082010   0104
+source dq2get_dataset.sh   159113   02   20082010   0105
 
-source dq2get.sh   159179   01   20082010   0107
-#source dq2get.sh   159179   02   20082010  #### this data set has no files
+source dq2get_dataset.sh   159179   01   20082010   0107
+#source dq2get_dataset.sh   159179   02   20082010  #### this data set has no files
 
-#source dq2get.sh   159202   01   20082010  #### this data set has no files
-source dq2get.sh   159202   02   20082010   0108
+#source dq2get_dataset.sh   159202   01   20082010  #### this data set has no files
+source dq2get_dataset.sh   159202   02   20082010   0108
 
-source dq2get.sh   159203   01   20082010   0109
-source dq2get.sh   159203   02   20082010   0110
+source dq2get_dataset.sh   159203   01   20082010   0109
+source dq2get_dataset.sh   159203   02   20082010   0110
 
-#source dq2get.sh   159224   01   20082010  #### this data set has no files
-#source dq2get.sh   159224   02   20082010   0112  ### ALREADY DOWNLOADED !!!
+#source dq2get_dataset.sh   159224   01   20082010  #### this data set has no files
+#source dq2get_dataset.sh   159224   02   20082010   0112  ### ALREADY DOWNLOADED !!!

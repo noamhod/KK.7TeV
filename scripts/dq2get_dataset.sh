ab6c84cf567edb328eeb434e60516de5e6f3bb22
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-08-24 13:47:41 +0000

    prun scripts update

--- scripts/dq2get_dataset.sh
+++ scripts/dq2get_dataset.sh
@@ -4,13 +4,10 @@ prefix=user.hod.WZphys
 
 runnumber=$1
 
-subjob=$2
+day=d$2
 
-day=d$3
+hour=h$3
 
-hour=h$4
+dq2-list-files $prefix.$runnumber.$day.$hour/ > $prefix.$runnumber.$day.$hour.listfiles
 
-dq2-list-files $prefix.$runnumber.$subjob.$day.$hour/ > $prefix.$runnumber.$subjob.$day.$hour.listfiles
-#dq2-list-files $prefix.$runnumber.$subjob.$day.$hour/
-
-dq2-get $prefix.$runnumber.$subjob.$day.$hour/
+dq2-get $prefix.$runnumber.$day.$hour/

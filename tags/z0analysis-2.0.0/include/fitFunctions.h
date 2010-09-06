02cc82567a1c4bf7c3958122ce32a9a407ca928e
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-09-06 17:30:28 +0000

    change fit strategy

---
+++ tags/z0analysis-2.0.0/include/fitFunctions.h
@@ -0,0 +1,32 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+#ifndef FITFUNCTIONS_H
+#define FITFUNCTIONS_H
+
+double XMIN = 50000.;
+double XMAX = 120000.;
+double DATASCALE;
+double FCNINTEGRAL;
+
+bool custom_isnan(double var);
+double scale2data(TH1D* h);
+double exponential(double *x, double *par);
+double DoubleGaus(double x, double mean, double sigma1, double sigma2);
+double TripleGaus(double x, double mean, double sigma1, double sigma2, double sigma3);
+double BreitGaus(double *x, double *par);
+double BreitDoubleGaus(double *x, double *par);
+double BreitTripleGaus(double *x, double *par);
+double fitFunctionSB(double *x, double *par);
+double fitFunctionSB2(double *x, double *par);
+double fitFunctionS(double *x, double *par);
+double fitFunctionS2(double *x, double *par);
+double fitFunctionS3(double *x, double *par);
+double fitFunctionSBscaled(double *x, double *par);
+
+#endif
+

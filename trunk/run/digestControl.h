a07eb554dec57a33bc2e35fe87a94faa67762d51
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-09-14 18:30:45 +0000

    add roofit

---
+++ trunk/run/digestControl.h
@@ -0,0 +1,79 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+#include "basicIncludes.h"
+
+#define digestChains_cxx
+#include "digestChains.C"
+
+#define digestAnalysis_cxx
+#include "digestAnalysis.C"
+
+#ifndef DIGESTCONTROL_H
+#define DIGESTCONTROL_H
+
+class digestControl : public digestChains, public digestAnalysis
+{
+	public:
+		// from MakeClass
+		digestPhysics* m_digestPhys;
+		cutFlowHandler* m_cutFlowHandler;
+		
+		// pointers
+		TFile*          m_histfile;
+		TFile*          m_treefile;
+		
+		TDirectory*     m_dirAllCuts;
+		TDirectory* 	m_dirNoCuts;
+		TDirectory* 	m_dirCutFlow;
+		TDirectory* 	m_dirFit;
+		
+		digestAnalysis* m_digestAnalysis;
+		graphicObjects*  m_graphics;
+
+		// run control
+		Long64_t l64t_nentries;
+		Long64_t l64t_nbytes;
+		Long64_t l64t_nb;
+		Long64_t l64t_jentry;
+		Long64_t l64t_ientry;
+	
+		Long64_t l64t_mod;
+		Long64_t l64t_startEvent;
+		Long64_t l64t_stopEvent;
+		
+		// for matching candidates
+		vector<double> runNumber;
+		vector<double> lumiBlock;
+		vector<double> eventNumber;
+		vector<double> mass_cb;
+		vector<double> mass_id;
+		vector<double> mass_me;
+		vector<double> pT1;
+		vector<double> pT2;
+	
+	public:
+		digestControl();
+		~digestControl();
+		void   initialize();
+		void   finalize();
+		
+		void resetLastCut2Hist(string sLastCut2Hist = "GRL") {m_digestAnalysis->m_sLastCut2Hist = sLastCut2Hist;}
+		
+		void   book();
+		void   draw();
+		void   analyze();
+		void   loop(Long64_t startEvent = 0, Long64_t stopAfterNevents = kMaxLong64);
+		
+		void getCandidates(string sFilePath);
+		bool matchCandidates();
+		void loopCandidates();
+
+	private:
+};
+#endif
+

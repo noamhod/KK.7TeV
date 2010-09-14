0b97884686c3b901789582302a829983d445d120
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-09-14 18:03:50 +0000

    add roofit

---
+++ tags/z0analysis-2.0.1/run/digestControlMain.C
@@ -0,0 +1,41 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 26/07/2010 12:36 */
+/* * * * * * * * * * * */
+
+#include <TROOT.h>
+
+void prepare() // do not use alone
+{
+	gROOT->Reset();
+	
+	gROOT->ProcessLine(".include ../include/");
+	gROOT->ProcessLine(".include ../src/");
+	gROOT->ProcessLine(".include ./");
+
+	gSystem->Load("libRooFit") ;
+
+	gSystem->Load( "libCintex.so" );
+	Cintex::Cintex::Enable();
+	gROOT->ProcessLine(".L Loader.C+");
+}
+
+void compile() // for re-compilation
+{
+	prepare();
+	
+	gROOT->ProcessLine(".L digestControl.C++");
+	
+	gROOT->ProcessLine("digestControl dc");
+}
+
+void load() // only for loading, if already compiled
+{
+	prepare();
+
+	gROOT->ProcessLine(".L digestControl_C.so");
+
+	gROOT->ProcessLine("digestControl dc");
+}
---
+++ tags/z0analysis-2.0.1/scripts/batch_dq2get.sh
@@ -0,0 +1,83 @@
+#!/bin/bash/
+
+source $HOME/setupGrid.sh
+
+#---------------------------------------------------
+# job submit    |  run    | sub |   day   |  hour  |
+# script        |  number | set |         |        |
+#---------------------------------------------------
+
+source dq2get_dataset.sh   00152166   01092010   1237 
+source dq2get_dataset.sh   00152214   01092010   1238
+source dq2get_dataset.sh   00152221   01092010   1239
+source dq2get_dataset.sh   00152345   01092010   1240
+source dq2get_dataset.sh   00152409   01092010   1241
+source dq2get_dataset.sh   00152441   01092010   1242
+source dq2get_dataset.sh   00152508   01092010   1243
+source dq2get_dataset.sh   00152777   01092010   1244
+source dq2get_dataset.sh   00152844   01092010   1245
+source dq2get_dataset.sh   00152845   01092010   1246
+source dq2get_dataset.sh   00152878   01092010   1246
+source dq2get_dataset.sh   00152933   01092010   1247
+source dq2get_dataset.sh   00152994   01092010   1248
+source dq2get_dataset.sh   00153030   01092010   1249
+source dq2get_dataset.sh   00153134   01092010   1250
+source dq2get_dataset.sh   00153136   01092010   1251
+source dq2get_dataset.sh   00153159   01092010   1252
+source dq2get_dataset.sh   00153200   01092010   1254
+source dq2get_dataset.sh   00153565   01092010   1255
+source dq2get_dataset.sh   00153599   01092010   1256
+source dq2get_dataset.sh   00154810   01092010   1258
+source dq2get_dataset.sh   00154813   01092010   1259
+source dq2get_dataset.sh   00154815   01092010   1300
+source dq2get_dataset.sh   00154817   01092010   1301
+source dq2get_dataset.sh   00154822   01092010   1302
+source dq2get_dataset.sh   00155073   01092010   1304
+source dq2get_dataset.sh   00155112   01092010   1305
+source dq2get_dataset.sh   00155116   01092010   1307
+source dq2get_dataset.sh   00155118   01092010   1308
+
+# 0
+###############################################################
+# 1
+
+source dq2get_dataset.sh   00155160   01092010   1310
+source dq2get_dataset.sh   00155228   01092010   1311
+source dq2get_dataset.sh   00155280   01092010   1312
+source dq2get_dataset.sh   00155569   01092010   1314
+source dq2get_dataset.sh   00155634   01092010   1315
+source dq2get_dataset.sh   00155669   01092010   1316 
+source dq2get_dataset.sh   00155678   01092010   1317
+source dq2get_dataset.sh   00155697   01092010   1319
+source dq2get_dataset.sh   00156682   01092010   1321
+source dq2get_dataset.sh   00158045   01092010   1322
+source dq2get_dataset.sh   00158116   01092010   1323
+
+# 1
+###############################################################
+# 2
+
+source dq2get_dataset.sh   00158269   01092010   1325 
+source dq2get_dataset.sh   00158299   01092010   1326
+source dq2get_dataset.sh   00158392   01092010   1327
+source dq2get_dataset.sh   00158443   01092010   1328
+source dq2get_dataset.sh   00158466   01092010   1329
+source dq2get_dataset.sh   00158545   01092010   1329
+source dq2get_dataset.sh   00158548   01092010   1330
+source dq2get_dataset.sh   00158549   01092010   1333
+source dq2get_dataset.sh   00158582   01092010   1334
+source dq2get_dataset.sh   00158632   01092010   1337
+source dq2get_dataset.sh   00158801   01092010   1338
+source dq2get_dataset.sh   00158975   01092010   1339
+source dq2get_dataset.sh   00159041   01092010   1342
+
+# 2
+###############################################################
+# 3
+
+source dq2get_dataset.sh   00159086   01092010   1343
+source dq2get_dataset.sh   00159113   01092010   1345
+source dq2get_dataset.sh   00159179   01092010   1347
+#source dq2get_dataset.sh   00159202   01092010
+#source dq2get_dataset.sh   00159203   01092010
+source dq2get_dataset.sh   00159224   01092010   1348

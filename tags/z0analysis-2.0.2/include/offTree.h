9b67e555ff4d2f8f41a0eae41c1b80bae0ad30e4
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-09-15 16:25:29 +0000

    prepare for 3ipb run

---
+++ tags/z0analysis-2.0.2/include/offTree.h
@@ -0,0 +1,120 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+#include "basicIncludes.h"
+
+#ifndef OFFTREE_H
+#define OFFTREE_H
+
+class offTree : public utilities
+{
+	public:
+		// pointers
+		physics* m_phys;
+		TFile*   m_treeFile;
+		TTree*   m_tree;
+		
+		// check
+		Bool_t emptyEvent;
+		
+		// event info branches
+		int RunNumber;
+		int lbn;
+		int EventNumber;
+		
+		// event preselection
+		int isGRL;
+		int L1_MU6;
+		int EF_mu10;
+		int EF_mu10_MSonly;
+		int EF_mu10_passHLT;
+		int EF_mu13;
+		int EF_mu15;
+		int EF_mu20;
+		int EF_mu20_MSonly;
+		int EF_mu20_passHLT;
+		int EF_mu4;
+		int EF_mu4_MSonly;
+		int EF_mu4_passHLT;
+		int EF_mu6;
+		int EF_mu6_MSonly;
+		int EF_mu6_passHLT;
+		
+		// mu_staco
+		Int_t          mu_staco_n;
+		vector<float>* mu_staco_px;
+		vector<float>* mu_staco_py;
+		vector<float>* mu_staco_pz;
+		vector<float>* mu_staco_E;
+		vector<float>* mu_staco_m;
+		vector<float>* mu_staco_charge;
+		vector<float>* mu_staco_pt;
+		vector<float>* mu_staco_ptcone20;
+		vector<float>* mu_staco_ptcone30;
+		vector<float>* mu_staco_ptcone40;
+		vector<float>* mu_staco_eta;
+		vector<float>* mu_staco_d0_exPV;
+		vector<float>* mu_staco_z0_exPV;
+		vector<float>* mu_staco_me_qoverp;
+		vector<float>* mu_staco_id_qoverp;
+		vector<float>* mu_staco_me_theta;
+		vector<float>* mu_staco_id_theta;
+		vector<int>*   mu_staco_isCombinedMuon;
+		vector<int>*   mu_staco_nSCTHits;
+		vector<int>*   mu_staco_nPixHits;
+		vector<unsigned short>* mu_staco_allauthor;
+		vector<int>*   mu_staco_author;
+		
+		
+		// muid
+		Int_t          mu_muid_n;
+		vector<float>* mu_muid_px;
+		vector<float>* mu_muid_py;
+		vector<float>* mu_muid_pz;
+		vector<float>* mu_muid_E;
+		vector<float>* mu_muid_m;
+		vector<float>* mu_muid_charge;
+		vector<float>* mu_muid_pt;
+		vector<float>* mu_muid_ptcone20;
+		vector<float>* mu_muid_ptcone30;
+		vector<float>* mu_muid_ptcone40;
+		vector<float>* mu_muid_eta;
+		vector<float>* mu_muid_d0_exPV;
+		vector<float>* mu_muid_z0_exPV;
+		vector<float>* mu_muid_me_qoverp;
+		vector<float>* mu_muid_id_qoverp;
+		vector<float>* mu_muid_me_theta;
+		vector<float>* mu_muid_id_theta;
+		vector<int>*   mu_muid_isCombinedMuon;
+		vector<int>*   mu_muid_nSCTHits;
+		vector<int>*   mu_muid_nPixHits;
+		vector<unsigned short>* mu_muid_allauthor;
+		vector<int>*   mu_muid_author;
+		
+		// vertex variables
+		Int_t          vxp_n;
+		vector<float>* vxp_z;
+		vector<float>* vxp_z_err;
+		vector<int>*   vxp_nTracks;
+		vector<int>*   vxp_type;
+	
+	public:
+		offTree();
+		offTree(physics* phys, TFile* treeFile);
+		~offTree();
+		
+		void setBranches();
+		void fill();
+		void fill(int isGrl);
+		void reset();
+		void write();
+
+	private:
+
+};
+#endif
+
---
+++ tags/z0analysis-2.0.2/run/local_offline_run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash/
+
+# the data set
+#datasetfiles=/tmp/hod/group10.phys-sm.data10_7TeV.00159224.physics_MuonswBeam.recon.ESD.x30.WZphys.100612.02.D3PD.D3PD._01067.root
+datasetfiles=$1
+
+
+# the GRL version
+#grlvs=00-00-84
+
+
+# add to LD_LIBRARY_PATH
+ispwdld=`echo $LD_LIBRARY_PATH | grep $PWD`
+if [ "$ispwdld" = "" ] ; then
+   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$PWD
+fi
+isgrlld=`echo $LD_LIBRARY_PATH | grep $PWD/GoodRunsLists-$grlvs/StandAlone`
+if [ "$isgrlld" = "" ] ; then
+   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$PWD/GoodRunsLists-$grlvs/StandAlone
+fi
+
+
+# date
+dateandhour=`date +d%d%m%Y.h%H%M`
+
+
+# remove the arlier outputs
+rm -f offlineinput.txt
+rm -f offlineWZphys.root
+rm -f Loader_C.so
+rm -f Loader_C.d
+
+# get the latest grl and cut flow
+cp -u ../conf/cutFlow.cuts .
+#cp -u ../conf/Z_GRL_152844-159224.xml .
+
+
+# make usable
+chmod 777 cutFlow.cuts
+#chmod 777 Z_GRL_152844-159224.xml
+
+
+# submit the local run
+echo $datasetfiles > offlineinput.txt;  root.exe offlineGridControlRun.C;

67ecc6a86be3222adddfc09fa47608f9176ff02b
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-09-16 22:09:36 +0000

    bug fix

---
+++ trunk/src/digestAnalysis.C
@@ -0,0 +1,482 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+#include "digestAnalysis.h"
+
+digestAnalysis::digestAnalysis()
+{
+	initialize();
+}
+
+digestAnalysis::digestAnalysis(digestPhysics* digestPhysics, graphicObjects* graphicobjs, cutFlowHandler* cutFlowHandler, TFile* treeFile, string sLastCut2Hist)
+{
+	initialize();
+
+	m_digestPhys = digestPhysics;
+
+	m_treeFile = treeFile;
+
+	m_cutFlowHandler = cutFlowHandler;
+	m_cutFlowMapSVD  = m_cutFlowHandler->getCutFlowMapSVDPtr();
+	m_cutFlowOrdered = m_cutFlowHandler->getCutFlowOrderedMapPtr();
+	m_cutFlowNumbers = m_cutFlowHandler->getCutFlowNumbersMapPtr();
+	
+	// cut flow has been read out already
+	initSelectionCuts(m_cutFlowMapSVD, m_cutFlowOrdered);
+
+	m_graphicobjs = graphicobjs;
+	
+	m_fit = new fit();
+	
+	m_sLastCut2Hist = sLastCut2Hist;
+}
+
+digestAnalysis::~digestAnalysis()
+{
+
+}
+
+void digestAnalysis::initialize()
+{
+
+}
+
+void digestAnalysis::finalize()
+{
+
+}
+
+void digestAnalysis::fitter()
+{
+	string lastCutName = m_sLastCut2Hist;
+
+	// clone the last wanted histogram to preform the fit on:
+	m_graphicobjs->h1_imassFinal = (TH1D*)m_graphicobjs->hmap_cutFlow_imass->operator[]("imass."+lastCutName)->Clone("imassFinal");
+	double yields[2];
+	
+	///////////////////////////////////////////////////////////////
+	// Preform the fit ////////////////////////////////////////////
+	m_fit->minimize( false, m_graphicobjs->h1_imassFinal, yields );
+	///////////////////////////////////////////////////////////////
+					
+	cout << "\nyields[0] = " <<  yields[0] << endl;
+	cout << "yields[1] = " <<  yields[1] << "\n" << endl;
+	
+	//m_fGuess  = (TF1*)m_fit->m_fGuess->Clone();
+	//m_fGuess  = m_fit->m_fGuess;
+	//m_fFitted = (TF1*)m_fit->m_fFitted->Clone();
+	//m_fFitted = m_fit->m_fFitted;
+}
+
+
+/*
+void digestAnalysis::executeTree(bool isendofrun)
+{
+	if(isendofrun) cout << "--- !!! END OF RUN !!! ---" << endl;
+}
+*/
+
+void digestAnalysis::executeBasic()
+{
+	// local variables
+	TMapii      mupairMap;
+	TVectorP2VL pmu;
+
+	double im;
+	double pTa;
+	double pTb;
+	double etaa;
+	double etab;
+	double costh;
+	//double d0exPVa;
+	//double z0exPVa;
+	//double d0exPVb;
+	//double z0exPVb;
+	double cosmicCosth;
+
+	// build vector of the muons TLorentzVector
+	for(int n=0 ; n<(int)m_digestPhys->mu_staco_n ; n++)
+	{
+		pmu.push_back( new TLorentzVector() );
+		pmu[n]->SetPx( m_digestPhys->mu_staco_px->at(n) );
+		pmu[n]->SetPy( m_digestPhys->mu_staco_py->at(n) );
+		pmu[n]->SetPz( m_digestPhys->mu_staco_pz->at(n) );
+		pmu[n]->SetE( m_digestPhys->mu_staco_E->at(n) );
+	}
+
+	// build the map of the good muon pairs	
+	for(int n=0 ; n<(int)pmu.size() ; n++)
+	{
+		for(int m=0 ; m<(int)pmu.size() ; m++)
+		{
+			// dont pair with itself
+			if( m==n ) continue;
+
+			// remove overlaps
+			if( removeOverlaps(mupairMap, n, m) ) continue;
+
+			// now can insert dimuon into the index map (all the final selection criteria)
+			b_isGRL = m_digestPhys->isGRL;
+			if( b_isGRL == 1 ) // this is a cut and not overlap removal
+			{
+				if( m_digestPhys->L1_MU6 == 1 ) // this is also a cut and not overlap removal
+				{
+					buildMuonPairMap( mupairMap,
+					pmu[n], m_digestPhys->mu_staco_charge->at(n), m_digestPhys->mu_staco_d0_exPV->at(n), m_digestPhys->mu_staco_z0_exPV->at(n), n,
+					pmu[m], m_digestPhys->mu_staco_charge->at(m), m_digestPhys->mu_staco_d0_exPV->at(m), m_digestPhys->mu_staco_z0_exPV->at(m), m );
+				}
+			}
+
+			// check before cuts
+			if(m_digestPhys->mu_staco_charge->at(n) * m_digestPhys->mu_staco_charge->at(m)<0.)
+			{
+				cosmicCosth = cosThetaDimu( pmu[n], pmu[m] );
+				m_graphicobjs->h1_cosmicCosth->Fill( cosmicCosth );
+				
+				d0exPVa = m_digestPhys->mu_staco_d0_exPV->at(n);
+				d0exPVb = m_digestPhys->mu_staco_d0_exPV->at(m);
+				m_graphicobjs->h1_d0exPV->Fill(d0exPVa);
+				m_graphicobjs->h1_d0exPV->Fill(d0exPVb);
+
+				z0exPVa = m_digestPhys->mu_staco_z0_exPV->at(n);
+				z0exPVb = m_digestPhys->mu_staco_z0_exPV->at(m);
+				m_graphicobjs->h1_z0exPV->Fill(z0exPVa);
+				m_graphicobjs->h1_z0exPV->Fill(z0exPVb);
+			}
+		}
+	}
+
+	// get the pmuon pairs from the dimuon good pairs map
+	if(mupairMap.size()>0)
+	{
+		for(TMapii::iterator it=mupairMap.begin() ; it!=mupairMap.end() ; ++it)
+		{
+			int ai = it->first;
+			int bi = it->second;
+			
+			im = imass( pmu[ai], pmu[bi] );
+			pTa = pT( pmu[ai] );
+			pTb = pT( pmu[bi] );
+			etaa = eta( pmu[ai] );	
+			etab = eta( pmu[bi] );
+			costh = cosThetaCollinsSoper( 	pmu[ai], (double)m_digestPhys->mu_staco_charge->at(ai),
+			pmu[bi], (double)m_digestPhys->mu_staco_charge->at(bi) );
+			d0exPVa = m_digestPhys->mu_staco_d0_exPV->at(ai);
+			z0exPVa = m_digestPhys->mu_staco_z0_exPV->at(ai);
+			d0exPVb = m_digestPhys->mu_staco_d0_exPV->at(bi);
+			z0exPVb = m_digestPhys->mu_staco_z0_exPV->at(bi);
+			//cosmicCosth = cosThetaDimu( pmu[ai], pmu[bi] );
+
+			cout << "$$$$$$$$$ dimuon $$$$$$$$$" << endl;
+			cout << "\t im=" << im << endl;
+			cout << "\t pTa=" << pTa  << endl;
+			cout << "\t pTb=" << pTb  << endl;
+			cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n" << endl;
+
+			// fill the histos
+			if( m_digestPhys->mu_staco_charge->at(ai)<0. )
+			{
+				m_graphicobjs->h1_pT->Fill( pTa );
+				m_graphicobjs->h1_eta->Fill( etaa );
+			}
+			else
+			{
+				m_graphicobjs->h1_pT->Fill( pTb );
+				m_graphicobjs->h1_eta->Fill( etab );
+			}
+			
+			m_graphicobjs->h1_imass->Fill( im );
+			m_graphicobjs->h1_costh->Fill( costh );
+		}
+	}
+
+	// re-initialize
+	if(mupairMap.size()>0)    mupairMap.clear();
+	if(pmu.size()>0)          pmu.clear();
+}
+
+
+void digestAnalysis::executeAdvanced()
+{
+
+/*
+	// stupid example
+	if(m_muid->getNParticles()>0)
+	{
+		m_muid->setParticle(0);
+	}
+
+	// stupid example
+	if(m_digestPhys->mu_staco_n>0)
+	{
+		for(int n=0 ; n<(int)m_mustaco->getNParticles() ; n++)
+		{
+			m_mustaco->setParticle(n);
+			m_graphicobjs->h2_xyVertex->Fill( m_mustaco->pvVec->X(), m_mustaco->pvVec->Y() );
+		}
+	}
+*/
+}
+
+void digestAnalysis::executeCutFlow()
+{
+	///////////////////////////////////////////
+	// do not skip this for correct counting //
+	m_cutFlowHandler->nAllEvents++; ///////////
+	///////////////////////////////////////////
+
+	// local variables
+	TVectorP2VL	pmu;
+	int         iVtx = 0;
+
+	//////////////////////////////////////////////////////////////////
+	// build vector of the muons TLorentzVector //////////////////////
+	for(int n=0 ; n<(int)m_digestPhys->mu_staco_n ; n++)
+	{
+		pmu.push_back( new TLorentzVector() );
+		pmu[n]->SetPx( m_digestPhys->mu_staco_px->at(n) );
+		pmu[n]->SetPy( m_digestPhys->mu_staco_py->at(n) );
+		pmu[n]->SetPz( m_digestPhys->mu_staco_pz->at(n) );
+		pmu[n]->SetE(  m_digestPhys->mu_staco_E->at(n)  );
+	}
+	///////////////////////////////////////////////////////////////////
+	
+	
+	///////////////////////////////////////
+	int ai=0, bi=1; ///////////////////////
+	///////////////////////////////////////
+	
+	
+
+	// calculate the necessary variables
+	current_imass    = imass(pmu[ai],pmu[bi]);
+	current_cosTheta = cosThetaCollinsSoper( pmu[ai], (double)m_digestPhys->mu_staco_charge->at(ai),
+	pmu[bi], (double)m_digestPhys->mu_staco_charge->at(bi) );
+	current_mu_pT    = (m_digestPhys->mu_staco_charge->at(ai)<0) ? m_digestPhys->mu_staco_pt->at(ai) : m_digestPhys->mu_staco_pt->at(bi);
+	current_mu_eta   = (m_digestPhys->mu_staco_charge->at(ai)<0) ? m_digestPhys->mu_staco_eta->at(ai) : m_digestPhys->mu_staco_eta->at(bi);
+	
+	// event level
+	runnumber  = m_digestPhys->RunNumber;
+	lumiblock  = m_digestPhys->lbn;
+	isL1MU6    = m_digestPhys->L1_MU6;
+	isGRL      = m_digestPhys->isGRL;
+	
+	// deprecated !!!
+	d0exPVa = m_digestPhys->mu_staco_d0_exPV->at(ai);
+	z0exPVa = m_digestPhys->mu_staco_z0_exPV->at(ai);
+	d0exPVb = m_digestPhys->mu_staco_d0_exPV->at(bi);
+	z0exPVb = m_digestPhys->mu_staco_z0_exPV->at(bi);
+	
+	// primary vertex:
+	// at least one primary vtx passes the z selection
+	nPVtracksPtr = m_digestPhys->vxp_nTracks; // number of tracks > 2
+	nPVtypePtr   = m_digestPhys->vxp_type;    // ==1
+	PVz0Ptr      = m_digestPhys->vxp_z;       // = absolute z position of primary vertex < 150mm
+	PVz0errPtr   = m_digestPhys->vxp_z_err;   // = error
+	
+	// combined muon ?
+	isMuaComb  = m_digestPhys->mu_staco_isCombinedMuon->at(ai);
+	isMubComb  = m_digestPhys->mu_staco_isCombinedMuon->at(bi);	
+	
+	// inner detector hits
+	nSCThitsMua  = m_digestPhys->mu_staco_nSCTHits->at(ai); //  SCT hits >=4
+	nSCThitsMub  = m_digestPhys->mu_staco_nSCTHits->at(bi); //  SCT hits >=4
+	nPIXhitsMua  = m_digestPhys->mu_staco_nPixHits->at(ai); // pixel hits >=1
+	nPIXhitsMub  = m_digestPhys->mu_staco_nPixHits->at(bi); // pixel hits >=1
+	nIDhitsMua   = nSCThitsMua+nPIXhitsMua; // pixel+SCT hits >=5
+	nIDhitsMub   = nSCThitsMub+nPIXhitsMub; // pixel+SCT hits >=5
+	
+	// ID - MS pT matching: pT=|p|*sin(theta), qOp=charge/|p|
+	me_qOp_a   = m_digestPhys->mu_staco_me_qoverp->at(ai);
+	id_qOp_a   = m_digestPhys->mu_staco_id_qoverp->at(ai);
+	me_theta_a = m_digestPhys->mu_staco_me_theta->at(ai);
+	id_theta_a = m_digestPhys->mu_staco_id_theta->at(ai);
+	me_qOp_b   = m_digestPhys->mu_staco_me_qoverp->at(bi);
+	id_qOp_b   = m_digestPhys->mu_staco_id_qoverp->at(bi);
+	me_theta_b = m_digestPhys->mu_staco_me_theta->at(bi);
+	id_theta_b = m_digestPhys->mu_staco_id_theta->at(bi);
+	
+	// impact parameter
+	impPrmZ0 = m_digestPhys->mu_staco_z0_exPV->at(ai);
+	impPrmD0 = m_digestPhys->mu_staco_d0_exPV->at(ai);
+	
+	// isolation
+	mu_pTa   = m_digestPhys->mu_staco_pt->at(ai);
+	mu_pTb   = m_digestPhys->mu_staco_pt->at(bi);
+	pTcone20a = m_digestPhys->mu_staco_ptcone20->at(ai);
+	pTcone20b = m_digestPhys->mu_staco_ptcone20->at(bi);
+	pTcone30a = m_digestPhys->mu_staco_ptcone30->at(ai);
+	pTcone30b = m_digestPhys->mu_staco_ptcone30->at(bi);
+	pTcone40a = m_digestPhys->mu_staco_ptcone40->at(ai);
+	pTcone40b = m_digestPhys->mu_staco_ptcone40->at(bi);
+	
+	// charge
+	mu_charge_a = m_digestPhys->mu_staco_charge->at(ai);
+	mu_charge_b = m_digestPhys->mu_staco_charge->at(bi);
+	
+	
+	TMapsd values2fill;
+	values2fill.insert( make_pair( "imass",current_imass ) );
+	values2fill.insert( make_pair( "pT",   current_mu_pT ) );
+	
+	bool passCutFlow    = true;
+	bool passCurrentCut = true;
+	// fill the cut flow, stop at the relevant cut each time.
+	// the cut objects don't have to be "correctly" ordered
+	// since it is done by the loop on the ordered cut flow map
+	int cutFlowMapSize = (int)m_cutFlowOrdered->size();
+	int counter = 0;
+	for(TMapds::iterator ii=m_cutFlowOrdered->begin() ; ii!=m_cutFlowOrdered->end() ; ++ii)
+	{
+		counter++;
+		string sorderedcutname = ii->second;
+
+		if(sorderedcutname=="oppositeCharcge")
+		{
+			passCurrentCut = ( oppositeChargeCut((*m_cutFlowMapSVD)[sorderedcutname][0], mu_charge_a, mu_charge_b) ) ? true : false;
+		}
+
+		if(sorderedcutname=="GRL")
+		{
+			passCurrentCut = ( isGRLCut((*m_cutFlowMapSVD)[sorderedcutname][0], isGRL) ) ? true : false;
+		}
+
+		if(sorderedcutname=="L1_MU6")
+		{
+			passCurrentCut = ( isL1_MU6Cut((*m_cutFlowMapSVD)[sorderedcutname][0], isL1MU6) ) ? true : false;
+		}		
+
+		if(sorderedcutname=="imass")
+		{
+			passCurrentCut = ( imassCut((*m_cutFlowMapSVD)[sorderedcutname][0], pmu[ai], pmu[bi]) ) ? true : false;
+		}
+
+		if(sorderedcutname=="pT")
+		{
+			passCurrentCut = ( pTCut((*m_cutFlowMapSVD)[sorderedcutname][0], pmu[ai], pmu[bi]) ) ? true : false;
+		}
+
+		if(sorderedcutname=="eta")
+		{
+			passCurrentCut = ( etaCut((*m_cutFlowMapSVD)[sorderedcutname][0], pmu[ai], pmu[bi]) ) ? true : false;
+		}
+
+		if(sorderedcutname=="cosThetaDimu")
+		{
+			passCurrentCut = ( cosThetaDimuCut((*m_cutFlowMapSVD)[sorderedcutname][0], pmu[ai], pmu[bi]) ) ? true : false;
+		}
+
+		if(sorderedcutname=="d0")
+		{
+			passCurrentCut = ( d0Cut((*m_cutFlowMapSVD)[sorderedcutname][0], d0exPVa, d0exPVb) ) ? true : false;
+		}
+
+		if(sorderedcutname=="z0")
+		{
+			passCurrentCut = ( z0Cut((*m_cutFlowMapSVD)[sorderedcutname][0], z0exPVa, z0exPVb) ) ? true : false;
+		}
+		
+		if(sorderedcutname=="PV")
+		{
+			double cutval1 = (*m_cutFlowMapSVD)[sorderedcutname][0];
+			double cutval2 = (*m_cutFlowMapSVD)[sorderedcutname][1];
+			double cutval3 = (*m_cutFlowMapSVD)[sorderedcutname][2];
+			passCurrentCut = ( primaryVertexCut(cutval1,cutval2,cutval3, nPVtracksPtr, nPVtypePtr, PVz0Ptr, PVz0errPtr, iVtx) ) ? true : false;
+		}
+		
+		if(sorderedcutname=="isCombMu")
+		{
+			passCurrentCut = ( isCombMuCut((*m_cutFlowMapSVD)[sorderedcutname][0],isMuaComb,isMubComb) ) ? true : false;
+		}
+		
+		/*
+		if(sorderedcutname=="idHits")
+		{
+			double cutval1 = (*m_cutFlowMapSVD)[sorderedcutname][0];
+			double cutval2 = (*m_cutFlowMapSVD)[sorderedcutname][1];
+			double cutval3 = (*m_cutFlowMapSVD)[sorderedcutname][2];
+			passCurrentCut = ( nIDhitsCut( cutval1,cutval2,cutval3,nSCThits,nPIXhits ) ) ? true : false;
+		}
+		*/
+		
+		if(sorderedcutname=="isolation40")
+		{
+			passCurrentCut =
+			(
+			pairXXisolation((*m_cutFlowMapSVD)[sorderedcutname][0],"isolation40",mu_pTa,mu_pTb,pTcone40a,pTcone40b)
+			) ? true : false;
+		}
+		
+		if(sorderedcutname=="pTmatchingRatio")
+		{
+			double cutval1 = (*m_cutFlowMapSVD)[sorderedcutname][0];
+			double cutval2 = (*m_cutFlowMapSVD)[sorderedcutname][1];
+			passCurrentCut =
+			(
+			pTmatchingRatioCut( cutval1,cutval2,
+			me_qOp_a,me_theta_a,id_qOp_a,id_theta_a,
+			me_qOp_b,me_theta_b,id_qOp_b,id_theta_b)
+			) ? true : false;
+		}
+
+		if(sorderedcutname=="pTmatchingAbsDiff")
+		{
+			double cutval1 = (*m_cutFlowMapSVD)[sorderedcutname][0];
+			passCurrentCut =
+			(
+			pTmatchingAbsDiffCut( cutval1,
+			me_qOp_a,me_theta_a,id_qOp_a,id_theta_a,
+			me_qOp_b,me_theta_b,id_qOp_b,id_theta_b)
+			) ? true : false;
+		}
+
+		passCutFlow = (passCurrentCut  &&  passCutFlow) ? true : false;
+		if(passCutFlow)
+		{
+			//////////////////////////////////////////////////////
+			// for the cut flow: /////////////////////////////////
+			m_graphicobjs->hmap_cutFlow_imass->operator[]("imass."+sorderedcutname)->Fill( values2fill["imass"] );
+			m_graphicobjs->hmap_cutFlow_pT->operator[]("pT."+sorderedcutname)->Fill( values2fill["pT"] );
+			//////////////////////////////////////////////////////
+
+			//////////////////////////////////////////////////////
+			// count the numbers: ////////////////////////////////
+			m_cutFlowNumbers->operator[](sorderedcutname) ++; ////
+			//////////////////////////////////////////////////////
+
+			///////////////////////////////////////////////////////////
+			// fill the "allCuts" histograms only after the last cut 
+			if(counter==cutFlowMapSize)
+			{
+				// for the final histograms:
+				// i.e., not the curFlow histos
+				m_graphicobjs->h1_eta->Fill( current_mu_eta );
+				m_graphicobjs->h1_costh->Fill( current_cosTheta );
+				m_graphicobjs->h1_pT->Fill( current_mu_pT );
+				m_graphicobjs->h1_imass->Fill( current_imass );
+				
+				cout << "$$$$$$$$$ dimuon $$$$$$$$$" << endl;
+				cout << "\t im=" << current_imass << endl;
+				cout << "\t pTmu=" << current_mu_pT  << endl;
+				cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n" << endl;
+				
+				// fill the xVector for the ML fit:
+				m_fit->fillXvec( current_imass );
+			}
+			///////////////////////////////////////////////////////////
+		} // end if(passCutFlow)
+	} // end for(m_cutFlowOrdered)
+
+	// re-initialize
+	if(pmu.size()>0)       pmu.clear();
+}
+
+void digestAnalysis::write()
+{
+	//m_treeFile->cd();
+	//m_offTreeDigest->write();
+}
---
+++ trunk/src/fitRooFit.C
@@ -0,0 +1,153 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+#ifdef fitRooFit_cxx
+#include "fitRooFit.h"
+
+
+fitRooFit::fitRooFit()
+{
+
+}
+
+fitRooFit::fitRooFit(vector<double>* x)
+{	
+	m_xVecPtr = x;
+}
+
+fitRooFit::~fitRooFit()
+{
+
+}
+
+void fitRooFit::minimize(bool signal_only, TH1D* h, double* yields)
+{
+	double XFULLMIN = 0.;
+	double XFULLMAX = 200000.;
+
+	double XFITMIN = XMIN;
+	double XFITMAX = XMAX;
+	
+	//////////////////////////////////////////////////////
+	// NOTE: the branch name has to be exactly the same as
+	// the observable name (RooRealVar)
+	//////////////////////////////////////////////////////
+
+	m_imassTree = new TTree("imassTree", "imassTree");
+	m_imassTree->Branch("imass", &m_imass);
+	for(int i=0 ; i<(int)m_xVecPtr->size() ; i++)
+	{
+		m_imass = m_xVecPtr->at(i);
+		m_imassTree->Fill();
+	}
+	m_imassTree->Write();
+
+	// --- Observable ---
+	// RooRealVar(const char* name, const char* title, Double_t value, Double_t minValue, Double_t maxValue, const char* unit = "")
+	RooRealVar imass("imass", "#hat{m} [MeV]", XFULLMIN, XFULLMAX);
+	imass.setRange("fitRange",XFITMIN,XFITMAX);
+	
+	
+	// -- SIGNAL
+	/*
+		RooVoigtian(const char* name, const char* title, RooAbsReal& _x, RooAbsReal& _mean, RooAbsReal& _width, RooAbsReal& _sigma, Bool_t doFast = kFALSE);
+		RooVoigtian is an efficient implementation of the convolution
+		of a Breit-Wigner with a Gaussian, making use of the complex error function.
+		RooFitCore provides two algorithms for the evaluation of the complex
+		error function (the default CERNlib C335 algorithm, and a faster,
+		look-up-table based method). By default, RooVoigtian employs the
+		default (CERNlib) algorithm. Select the faster algorithm either in
+		the constructor, or with the selectFastAlgorithm() method
+	*/
+	// --- Signal Parameters
+	RooRealVar gaussMean("gaussMean", "m_{Z^{0}}", 91000., XFULLMIN, XFULLMAX);
+	RooRealVar gaussSigma("gaussSigma", "Resolution", 5000., 0., 10000.);
+	RooRealVar breitWignerMean("breitWignerMean", "m_{Z^{0}}", 91000., XFULLMIN, XFULLMAX);
+	RooRealVar breitWignerGamma("breitWignerGamma", "#Gamma", 2500., 2000., 3000.);
+	// --- Build the convolution of the Gauss and Breit-Wigner PDFs ---
+	RooVoigtian BreitGaussSignal("BreitGauss", "Breit-Wigner #otimes Gauss PDF", imass, breitWignerMean, breitWignerGamma, gaussSigma);
+	
+	// --- BACKGROUND
+	//RooExponential(const char* name, const char* title, RooAbsReal& _x, RooAbsReal& _c)
+	// --- Background Parameters
+	RooRealVar expMeasure("expMeasure", "Exponent measure", -1.e-4, -10., 0.);
+	// --- Build the background exponential PDFs ---
+	RooExponential ExponentBG("ExponentBG", "Exponential BG", imass, expMeasure);
+	
+	
+	// --- SUM
+	// --- Construct signal+background PDF ---
+	double sbg = scale2data(h)/h->GetBinWidth(1);
+	double bg    = scale2bg(h)/h->GetBinWidth(1);
+	int nentries = (int)h->GetEntries();
+	RooRealVar nsig("nsig","#signal events",(int)sbg,0,nentries);
+	RooRealVar nbkg("nbkg","#background events",(int)bg,0,nentries);
+	RooAddPdf model("model", "BreitGaussSignal #oplus ExponentBG", RooArgList(BreitGaussSignal,ExponentBG), RooArgList(nsig,nbkg));
+	
+	// --- get the data set ---
+	RooRealVar boundaries("boundaries","boundaries",XFULLMIN,XFULLMAX);
+	RooDataSet* data = new RooDataSet("data", "data", m_imassTree, imass /*RooArgSet(imass, boundaries)*/);
+	
+	// --- Perform extended ML fit of composite PDF to data ---
+	model.fitTo(*data, Range("fitRange"),Extended(kTRUE), "etlrh");
+		
+	// --- Plot toy data and composite PDF overlaid ---
+	TCanvas* canv_imass_roofit = new TCanvas("imass_roofit","imass_roofit",600,400);
+	canv_imass_roofit->Draw();
+	canv_imass_roofit->cd();
+	RooPlot* frame = imass.frame();
+	RooBinning b((int)h->GetNbinsX(),XFULLMIN,XFULLMAX,"imassBins");
+	data->plotOn(frame, Binning(b), XErrorSize(0) /*, Invisible()*/);
+	model.plotOn(frame);
+	model.plotOn(frame,Components(BreitGaussSignal),LineColor(kRed),LineWidth(1));
+	model.plotOn(frame,Components(ExponentBG),LineStyle(kDashed),LineWidth(1));
+	
+	double leg_x1 = 0.765;
+	double leg_x2 = 0.919;
+	double leg_y1 = 0.376;
+	double leg_y2 = 0.922;
+	TLegend* leg_roofit = new TLegend(leg_x1, leg_y1*2., leg_x2, leg_y2);
+	TGraph* tgraph_dat = new TGraph();
+	TLine* tline_sig = new TLine();
+	tline_sig->SetLineWidth(1); tline_sig->SetLineColor(kRed); tline_sig->SetLineStyle(1);
+	TLine* tline_bg = new TLine();
+	tline_bg->SetLineWidth(1); tline_bg->SetLineColor(kBlue); tline_bg->SetLineStyle(kDashed);
+	TLine* tline_fit = new TLine();
+	tline_fit->SetLineWidth(2); tline_fit->SetLineColor(kBlue); tline_fit->SetLineStyle(1);
+	leg_roofit->AddEntry( tgraph_dat, "Data", "lep");
+	leg_roofit->AddEntry( tline_sig, "S", "L");
+	leg_roofit->AddEntry( tline_bg, "BG", "L");
+	leg_roofit->AddEntry( tline_fit, "ML fit", "L");
+	frame->addObject(leg_roofit);
+	
+	frame->SetMinimum(1.e-5);
+	
+	frame->Draw();
+	canv_imass_roofit->Update();
+	canv_imass_roofit->Write();
+	
+	
+	/*
+	the error bars for entries at low statistics are not 
+	symmetric: RooFit by default shows the 68% confidence interval for Poisson statistics (To be more precise
+	the intervals shown are ‘classic central ’ intervals as described in Table I of Cousins, Am. J. Phys. 63, 398 (1995)),
+	which are  generally asymmetric, especially at low statistics, and a more accurate reflection of the statistical 
+	uncertainty in each bin if the histogram contents arose from a Poisson process. You can choose to have the usual sqrt(N)
+	error shown by adding DataError(RooAbsData::SumW2) to the  data.plotOn() line. This option only affects the visualization
+	of the dataset.
+	*/
+	
+	cout << "breitWignerMean = " << breitWignerMean.getVal() << " +- " << breitWignerMean.getError() << endl;
+	cout << "breitWignerGamma = " << breitWignerGamma.getVal() << " +- " << breitWignerGamma.getError() << endl;
+	cout << "gaussSigma = " << gaussSigma.getVal() << " +- " << gaussSigma.getError() << endl;
+	cout << "expMeasure = " << expMeasure.getVal() << " +- " << expMeasure.getError() << endl;
+	cout << "nsig = " << nsig.getVal() << " +- " << nsig.getError() << endl;
+	cout << "nbkg = " << nbkg.getVal() << " +- " << nbkg.getError() << endl;
+}
+
+#endif
+

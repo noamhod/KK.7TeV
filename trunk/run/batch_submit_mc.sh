8c43e163610ffe29e81f1d884b18891fadeddf4f
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-09-27 23:02:13 +0000

    update panda_run script with nGBPerJob option

--- trunk/run/batch_submit_mc.sh
+++ trunk/run/batch_submit_mc.sh
@@ -7,7 +7,7 @@
 
  source panda_run.sh   mc   106047   group10.phys-sm.mc09_7TeV.106047.PythiaZmumu_no_filter.merge.AOD.e468_s765_s767_r1388_r1389.WZphys.100612.12.D3PD
  source panda_run.sh   mc   106044   group10.phys-sm.mc09_7TeV.106044.PythiaWmunu_no_filter.merge.AOD.e468_s765_s767_r1388_r1389.WZphys.100612.12.D3PD
- source panda_run.sh   mc   106052   group10.phys-sm.mc09_7TeV.106052.PythiaZtautau.merge.AOD.e468_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+# source panda_run.sh   mc   106052   group10.phys-sm.mc09_7TeV.106052.PythiaZtautau.merge.AOD.e468_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
  source panda_run.sh   mc   105861   group10.phys-sm.mc09_7TeV.105861.TTbar_PowHeg_Pythia.merge.AOD.e521_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
  source panda_run.sh   mc   108405   group10.phys-sm.mc09_7TeV.108405.PythiaB_bbmu15X.merge.AOD.e521_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
  source panda_run.sh   mc   106059   group10.phys-sm.mc09_7TeV.106059.PythiaB_ccmu15X.merge.AOD.e521_s765_s767_r1302_r1306.WZphys.100612.01.D3PD

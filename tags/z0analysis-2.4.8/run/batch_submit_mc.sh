0fa2edb1fd9f5ddd736fa5fbc68248af8be27ca8
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-12-10 20:16:00 +0000

    updated Afb occurance in analysis skeleton

---
+++ tags/z0analysis-2.4.8/run/batch_submit_mc.sh
@@ -0,0 +1,82 @@
+#/bin/bash/
+
+#-------------------------------------------
+# job submit       |  run    | sub | dataset
+# script           |  number | set | name
+#-------------------------------------------
+
+# Zmumu SM signal
+ source panda_run.sh   mc   106047   group10.phys-sm.mc09_7TeV.106047.PythiaZmumu_no_filter.merge.AOD.e468_s765_s767_r1388_r1389.WZphys.100612.12.D3PD
+
+# DYmumu SM signal
+ source panda_run.sh   mc   105477   group10.phys-sm.mc09_7TeV.105477.Pythia_DYmumu_75M120_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105478   group10.phys-sm.mc09_7TeV.105478.Pythia_DYmumu_120M250_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105479   group10.phys-sm.mc09_7TeV.105479.Pythia_DYmumu_250M400_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105480   group10.phys-sm.mc09_7TeV.105480.Pythia_DYmumu_400M600_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105481   group10.phys-sm.mc09_7TeV.105481.Pythia_DYmumu_600M800_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105482   group10.phys-sm.mc09_7TeV.105482.Pythia_DYmumu_800M1000_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105483   group10.phys-sm.mc09_7TeV.105483.Pythia_DYmumu_1000M1250_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105484   group10.phys-sm.mc09_7TeV.105484.Pythia_DYmumu_1250M1500_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.02.D3PD
+ source panda_run.sh   mc   105485   group10.phys-sm.mc09_7TeV.105485.Pythia_DYmumu_1500M1750_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105486   group10.phys-sm.mc09_7TeV.105486.Pythia_DYmumu_1750M2000_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105487   group10.phys-sm.mc09_7TeV.105487.Pythia_DYmumu_M2000_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+
+# DYtautau SM background
+ source panda_run.sh   mc   105488   group10.phys-sm.mc09_7TeV.105488.Pythia_DYtautau_75M120_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD	  
+ source panda_run.sh   mc   105489   group10.phys-sm.mc09_7TeV.105489.Pythia_DYtautau_120M250_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD	  
+ source panda_run.sh   mc   105490   group10.phys-sm.mc09_7TeV.105490.Pythia_DYtautau_250M400_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD	  
+ source panda_run.sh   mc   105491   group10.phys-sm.mc09_7TeV.105491.Pythia_DYtautau_400M600_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD	  
+ source panda_run.sh   mc   105492   group10.phys-sm.mc09_7TeV.105492.Pythia_DYtautau_600M800_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD	  
+ source panda_run.sh   mc   105493   group10.phys-sm.mc09_7TeV.105493.Pythia_DYtautau_800M1000_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD	  
+ source panda_run.sh   mc   105494   group10.phys-sm.mc09_7TeV.105494.Pythia_DYtautau_1000M1250_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105495   group10.phys-sm.mc09_7TeV.105495.Pythia_DYtautau_1250M1500_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.02.D3PD
+ source panda_run.sh   mc   105496   group10.phys-sm.mc09_7TeV.105496.Pythia_DYtautau_1500M1750_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD	  
+ source panda_run.sh   mc   105497   group10.phys-sm.mc09_7TeV.105497.Pythia_DYtautau_1750M2000_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD	  
+ source panda_run.sh   mc   105498   group10.phys-sm.mc09_7TeV.105498.Pythia_DYtautau_M2000_unfiltered.merge.AOD.e549_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+
+# SM background
+ source panda_run.sh   mc   106044   group10.phys-sm.mc09_7TeV.106044.PythiaWmunu_no_filter.merge.AOD.e468_s765_s767_r1388_r1389.WZphys.100612.12.D3PD
+ source panda_run.sh   mc   106052   group10.phys-sm.mc09_7TeV.106052.PythiaZtautau.merge.AOD.e468_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105861   group10.phys-sm.mc09_7TeV.105861.TTbar_PowHeg_Pythia.merge.AOD.e521_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   108405   group10.phys-sm.mc09_7TeV.108405.PythiaB_bbmu15X.merge.AOD.e521_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   106059   group10.phys-sm.mc09_7TeV.106059.PythiaB_ccmu15X.merge.AOD.e521_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+
+
+# Z'mumu signals
+ source panda_run.sh   mc   105601   group10.phys-sm.mc09_7TeV.105601.Pythia_Zprime_mumu_SSM1000.merge.AOD.e524_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105534   group10.phys-sm.mc09_7TeV.105534.Pythia_Zprime_mumu_SSM1250.merge.AOD.e524_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105625   group10.phys-sm.mc09_7TeV.105625.Pythia_Zprime_mumu_SSM1500.merge.AOD.e524_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105544   group10.phys-sm.mc09_7TeV.105544.Pythia_Zprime_mumu_SSM1750.merge.AOD.e524_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+ source panda_run.sh   mc   105349   group10.phys-sm.mc09_7TeV.105349.Pythia_Zprime_mumu_SSM2000.merge.AOD.e524_s765_s767_r1302_r1306.WZphys.100612.01.D3PD
+
+ 
+# di-boson WW / ZZ / WZ
+ source panda_run.sh   mc   107100   group10.phys-sm.mc09_7TeV.107100.AlpgenJimmyWWlnulnuNp0.merge.AOD.e536_s765_s767_r1302_r1306.WZphys.100612.02.D3PD
+ source panda_run.sh   mc   107101   group10.phys-sm.mc09_7TeV.107101.AlpgenJimmyWWlnulnuNp1.merge.AOD.e536_s765_s767_r1302_r1306.WZphys.100612.02.D3PD
+ source panda_run.sh   mc   107102   group10.phys-sm.mc09_7TeV.107102.AlpgenJimmyWWlnulnuNp2.merge.AOD.e536_s765_s767_r1302_r1306.WZphys.100612.02.D3PD
+ source panda_run.sh   mc   107103   group10.phys-sm.mc09_7TeV.107103.AlpgenJimmyWWlnulnuNp3.merge.AOD.e536_s765_s767_r1302_r1306.WZphys.100612.02.D3PD
+
+ source panda_run.sh   mc   106014   group10.phys-sm.mc09_7TeV.106014.gg2WW0240_JIMMY_WW_munumunu.merge.AOD.e530_s765_s767_r1302_r1306.WZphys.100612.03.D3PD
+ source panda_run.sh   mc   106016   group10.phys-sm.mc09_7TeV.106016.gg2WW0240_JIMMY_WW_munutaunu.merge.AOD.e530_s765_s767_r1302_r1306.WZphys.100612.03.D3PD
+ source panda_run.sh   mc   106017   group10.phys-sm.mc09_7TeV.106017.gg2WW0240_JIMMY_WW_taunutaunu.merge.AOD.e530_s765_s767_r1302_r1306.WZphys.100612.03.D3PD
+ source panda_run.sh   mc   106019   group10.phys-sm.mc09_7TeV.106019.gg2WW0240_JIMMY_WW_taunumunu.merge.AOD.e530_s765_s767_r1302_r1306.WZphys.100612.03.D3PD
+
+ source panda_run.sh   mc   105924   group10.phys-sm.mc09_7TeV.105924.McAtNlo_JIMMY_WpWm_munumunu.merge.AOD.e532_s765_s767_r1302_r1306.WZphys.100612.03.D3PD
+ source panda_run.sh   mc   105926   group10.phys-sm.mc09_7TeV.105926.McAtNlo_JIMMY_WpWm_munutaunu.merge.AOD.e532_s765_s767_r1302_r1306.WZphys.100612.03.D3PD
+ source panda_run.sh   mc   105927   group10.phys-sm.mc09_7TeV.105927.McAtNlo_JIMMY_WpWm_taunutaunu.merge.AOD.e532_s765_s767_r1302_r1306.WZphys.100612.03.D3PD
+
+ source panda_run.sh   mc   116000   group10.phys-sm.mc09_7TeV.116000.PythiaH200zzllnunu.merge.AOD.e550_s765_s767_r1302_r1306.WZphys.100612.20.D3PD/
+ source panda_run.sh   mc   116002   group10.phys-sm.mc09_7TeV.116002.PythiaH300zzllnunu.merge.AOD.e550_s765_s767_r1302_r1306.WZphys.100612.20.D3PD/
+ source panda_run.sh   mc   116004   group10.phys-sm.mc09_7TeV.116004.PythiaH400zzllnunu.merge.AOD.e550_s765_s767_r1302_r1306.WZphys.100612.20.D3PD/
+ source panda_run.sh   mc   116006   group10.phys-sm.mc09_7TeV.116006.PythiaH500zzllnunu.merge.AOD.e550_s765_s767_r1302_r1306.WZphys.100612.20.D3PD/
+ source panda_run.sh   mc   116008   group10.phys-sm.mc09_7TeV.116008.PythiaH600zzllnunu.merge.AOD.e550_s765_s767_r1302_r1306.WZphys.100612.20.D3PD/
+
+ source panda_run.sh   mc   109291   group10.phys-sm.mc09_7TeV.109291.Pythiazz4l.merge.AOD.e530_s765_s767_r1302_r1306.WZphys.100612.06.D3PD
+ source panda_run.sh   mc   105931   group10.phys-sm.mc09_7TeV.105931.McAtNlo_JIMMY_ZZ_llll.merge.AOD.e530_s765_s767_r1302_r1306.WZphys.100612.06.D3PD
+ source panda_run.sh   mc   105932   group10.phys-sm.mc09_7TeV.105932.McAtNlo_JIMMY_ZZ_llnunu.merge.AOD.e530_s765_s767_r1302_r1306.WZphys.100612.06.D3PD
+ source panda_run.sh   mc   106036   group10.phys-sm.mc09_7TeV.106036.McAtNlo_JIMMY_ZZ_2l2tau.merge.AOD.e530_s765_s767_r1302_r1306.WZphys.100612.06.D3PD
+ source panda_run.sh   mc   105930   group10.phys-sm.mc09_7TeV.105930.McAtNlo_JIMMY_ZZ_llqq.merge.AOD.e530_s765_s767_r1302_r1306.WZphys.100612.06.D3PD
+ source panda_run.sh   mc   107108   group10.phys-sm.mc09_7TeV.107108.AlpgenJimmyZZincllNp0.merge.AOD.e536_s765_s767_r1302_r1306.WZphys.100612.06.D3PD
+ source panda_run.sh   mc   107109   group10.phys-sm.mc09_7TeV.107109.AlpgenJimmyZZincllNp1.merge.AOD.e536_s765_s767_r1302_r1306.WZphys.100612.06.D3PD
+ source panda_run.sh   mc   107110   group10.phys-sm.mc09_7TeV.107110.AlpgenJimmyZZincllNp2.merge.AOD.e536_s765_s767_r1302_r1306.WZphys.100612.06.D3PD
+ source panda_run.sh   mc   107111   group10.phys-sm.mc09_7TeV.107111.AlpgenJimmyZZincllNp3.merge.AOD.e536_s765_s767_r1302_r1306.WZphys.100612.06.D3PD

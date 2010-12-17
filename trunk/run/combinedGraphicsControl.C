892605809a33c3ba28ed93532d094ec70923f6ac
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-12-17 00:14:13 +0000

    fix a bug in double muon selection and update the combined graphics

---
+++ trunk/run/combinedGraphicsControl.C
@@ -0,0 +1,82 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+#include "combinedGraphicsControl.h"
+
+
+combinedGraphicsControl::combinedGraphicsControl()
+{
+
+}
+
+combinedGraphicsControl::~combinedGraphicsControl()
+{
+	
+}
+
+void combinedGraphicsControl::initialize(string sAnalysisSelector, string muonSelector)
+{
+	string str = "";
+	
+	str = checkANDsetFilepath("PWD", "/../data/combinedGraphicsControl.root");
+	m_histfile = new TFile( str.c_str(), "RECREATE");
+	m_histfile->cd();
+
+	// read the cut flow (ownership: selection class which offlineAnalysis inherits from)
+	string sCutFlowFilePath  = checkANDsetFilepath("PWD", "/../conf/cutFlow.cuts");
+	string sPeriodsFilePath  = checkANDsetFilepath("PWD", "/../conf/dataPeriods.data");
+	
+	m_combinedGraphics = new combinedGraphics(sCutFlowFilePath, sPeriodsFilePath, "", sAnalysisSelector, muonSelector, m_histfile);
+}
+
+void  combinedGraphicsControl::finalize()
+{
+	m_histfile->Write();
+	m_histfile->Close();
+}
+
+void combinedGraphicsControl::execute()
+{
+	string dir  = "/data/hod/D3PDdigest/rel15_barrel_selection/";
+	string hDir = "allCuts";
+	
+	m_combinedGraphics->set_MCvsData(true, true, 5.e-5, 3.e+3, 0.075);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "imass", "#hat{m}_{#mu#mu} (TeV)", "#frac{dN}{d#hat{m}}");
+
+	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "etaSum", "#sum#eta", "#frac{dN}{d#sum#eta}");
+	
+	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_MCvsData(dir, "noCuts", "cosThetaDimu", "cos(#theta_{#mu#mu})", "#frac{dN}{dcos(#theta_{#mu#mu})}");
+	
+	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_MCvsData(dir, "noCuts", "cosThetaDimuAllCuts", "cos(#theta_{#mu#mu})", "#frac{dN}{dcos(#theta_{#mu#mu})}");
+	
+	m_combinedGraphics->set_MCvsData(true, true, 5.e-5, 3.e+3, 0.025);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "pT", "p_{T}(#mu^{-}) (TeV)", "#frac{dN}{dp_{T}}");
+	
+	m_combinedGraphics->set_MCvsData(true, true, 5.e-5, 3.e+3, 0.025);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "pT #mu^{+}", "p_{T}(#mu^{+}) (TeV)", "#frac{dN}{dp_{T}}");
+	
+	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "eta", "#eta(#mu^{-})", "#frac{dN}{d#eta}");
+	
+	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "eta #mu^{+}", "#eta(#mu^{+})", "#frac{dN}{d#eta}");
+	
+	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "pTratio_mu-", "p_{T}^{rat}(#mu^{-}) = p_{T}^{me}/p_{T}^{id}", "#frac{dN}{dp_{T}^{rat}}");
+	
+	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "pTratio_mu+", "p_{T}^{rat}(#mu^{+}) = p_{T}^{me}/p_{T}^{id}", "#frac{dN}{dp_{T}^{rat}}");
+	
+	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "pTdiff_mu-", "p_{T}^{diff}(#mu^{-}) = p_{T}^{me}-p_{T}^{id}", "#frac{dN}{dp_{T}^{diff}}");
+	
+	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_MCvsData(dir, hDir, "pTdiff_mu+", "p_{T}^{diff}(#mu^{+}) = p_{T}^{me}-p_{T}^{id}", "#frac{dN}{dp_{T}^{diff}}");
+}

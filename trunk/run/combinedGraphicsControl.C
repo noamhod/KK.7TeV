fffceeb9dee7bbede1ae9ade9468de4263f586b7
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-12-19 21:06:25 +0000

    update special Afb histogram

--- trunk/run/combinedGraphicsControl.C
+++ trunk/run/combinedGraphicsControl.C
@@ -44,39 +44,42 @@ void combinedGraphicsControl::execute()
 	string dir  = "/data/hod/D3PDdigest/rel15_barrel_selection/";
 	string hDir = "allCuts";
 	
-	m_combinedGraphics->set_MCvsData(true, true, 5.e-5, 3.e+3, 0.075);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "imass", "#hat{m}_{#mu#mu} (TeV)", "#frac{dN}{d#hat{m}}");
-
-	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "etaSum", "#sum#eta", "#frac{dN}{d#sum#eta}");
+	/*
+	// imass, etaSum and cos(theta_mumu) (dimu)
+	m_combinedGraphics->set_allMCvsData(true, true, 5.e-5, 3.e+3, 0.075);
+	m_combinedGraphics->draw_allMCvsData(dir, hDir, "imass", "#hat{m}_{#mu#mu} (TeV)", "#frac{dN}{d#hat{m}}");
+	m_combinedGraphics->set_allMCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_allMCvsData(dir, hDir, "etaSum", "#sum#eta", "#frac{dN}{d#sum#eta}");
+	m_combinedGraphics->set_allMCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_allMCvsData(dir, "noCuts", "cosThetaDimu", "cos(#theta_{#mu#mu})", "#frac{dN}{dcos(#theta_{#mu#mu})}");
+	m_combinedGraphics->set_allMCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_allMCvsData(dir, "noCuts", "cosThetaDimuAllCuts", "cos(#theta_{#mu#mu})", "#frac{dN}{dcos(#theta_{#mu#mu})}");
+	*/
 	
-	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
-	m_combinedGraphics->draw_MCvsData(dir, "noCuts", "cosThetaDimu", "cos(#theta_{#mu#mu})", "#frac{dN}{dcos(#theta_{#mu#mu})}");
+	// Afb
+	m_combinedGraphics->set_AfbMCvsData(false, false, -1.05, +1.05, 0.);
+	m_combinedGraphics->draw_AfbMCvsData(dir, hDir, "Afb", "#hat{m}_{#mu#mu} TeV", "A_{FB}#left(#hat{m}_{#mu#mu}#right)");
 	
-	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
-	m_combinedGraphics->draw_MCvsData(dir, "noCuts", "cosThetaDimuAllCuts", "cos(#theta_{#mu#mu})", "#frac{dN}{dcos(#theta_{#mu#mu})}");
+	/*
+	// pT and eta (mu-, mu+)
+	m_combinedGraphics->set_allMCvsData(true, true, 5.e-5, 3.e+3, 0.025);
+	m_combinedGraphics->draw_allMCvsData(dir, hDir, "pT", "p_{T}(#mu^{-}) (TeV)", "#frac{dN}{dp_{T}}");
+	m_combinedGraphics->set_allMCvsData(true, true, 5.e-5, 3.e+3, 0.025);
+	m_combinedGraphics->draw_allMCvsData(dir, hDir, "pT #mu^{+}", "p_{T}(#mu^{+}) (TeV)", "#frac{dN}{dp_{T}}");
+	m_combinedGraphics->set_allMCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_allMCvsData(dir, hDir, "eta", "#eta(#mu^{-})", "#frac{dN}{d#eta}");
+	m_combinedGraphics->set_allMCvsData(false, true, 5.e-5, 1.e+3, 0.);
+	m_combinedGraphics->draw_allMCvsData(dir, hDir, "eta #mu^{+}", "#eta(#mu^{+})", "#frac{dN}{d#eta}");
 	
-	m_combinedGraphics->set_MCvsData(true, true, 5.e-5, 3.e+3, 0.025);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "pT", "p_{T}(#mu^{-}) (TeV)", "#frac{dN}{dp_{T}}");
-	
-	m_combinedGraphics->set_MCvsData(true, true, 5.e-5, 3.e+3, 0.025);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "pT #mu^{+}", "p_{T}(#mu^{+}) (TeV)", "#frac{dN}{dp_{T}}");
-	
-	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "eta", "#eta(#mu^{-})", "#frac{dN}{d#eta}");
-	
-	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "eta #mu^{+}", "#eta(#mu^{+})", "#frac{dN}{d#eta}");
-	
-	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "pTratio_mu-", "p_{T}^{rat}(#mu^{-}) = p_{T}^{me}/p_{T}^{id}", "#frac{dN}{dp_{T}^{rat}}");
-	
-	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "pTratio_mu+", "p_{T}^{rat}(#mu^{+}) = p_{T}^{me}/p_{T}^{id}", "#frac{dN}{dp_{T}^{rat}}");
-	
-	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "pTdiff_mu-", "p_{T}^{diff}(#mu^{-}) = p_{T}^{me}-p_{T}^{id}", "#frac{dN}{dp_{T}^{diff}}");
-	
-	m_combinedGraphics->set_MCvsData(false, true, 5.e-5, 1.e+3, 0.);
-	m_combinedGraphics->draw_MCvsData(dir, hDir, "pTdiff_mu+", "p_{T}^{diff}(#mu^{+}) = p_{T}^{me}-p_{T}^{id}", "#frac{dN}{dp_{T}^{diff}}");
+	// pT ratio and diff (mu-, mu+)
+	m_combinedGraphics->set_sumMCvsData(false, true, 5.e-3, 1.e+3, 0., true);
+	m_combinedGraphics->draw_sumMCvsData(dir, hDir, "pTratio_mu-", "p_{T}^{rat}(#mu^{-}) = p_{T}^{me}/p_{T}^{id}", "#frac{dN}{dp_{T}^{rat}}");
+	m_combinedGraphics->set_sumMCvsData(false, true, 5.e-3, 1.e+3, 0., true);
+	m_combinedGraphics->draw_sumMCvsData(dir, hDir, "pTratio_mu+", "p_{T}^{rat}(#mu^{+}) = p_{T}^{me}/p_{T}^{id}", "#frac{dN}{dp_{T}^{rat}}");
+	m_combinedGraphics->set_sumMCvsData(false, true, 5.e-3, 1.e+3, 0., true);
+	m_combinedGraphics->draw_sumMCvsData(dir, hDir, "pTdiff_mu-", "p_{T}^{diff}(#mu^{-}) = p_{T}^{me}-p_{T}^{id}", "#frac{dN}{dp_{T}^{diff}}");
+	m_combinedGraphics->set_sumMCvsData(false, true, 5.e-3, 1.e+3, 0., true);
+	m_combinedGraphics->draw_sumMCvsData(dir, hDir, "pTdiff_mu+", "p_{T}^{diff}(#mu^{+}) = p_{T}^{me}-p_{T}^{id}", "#frac{dN}{dp_{T}^{diff}}");
+	*/
 }
+

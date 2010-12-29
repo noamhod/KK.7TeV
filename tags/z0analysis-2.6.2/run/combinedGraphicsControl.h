60305782670b9fb8f3e942928ae6db56cee43ca7
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2010-12-29 15:34:51 +0000

    added eff maps

---
+++ tags/z0analysis-2.6.2/run/combinedGraphicsControl.h
@@ -0,0 +1,39 @@
+/* * * * * * * * * * * */
+/* created by Naom Hod */
+/* noam.hod@cern.ch    */
+/* noam.com@gmail.com  */
+/* on 23/07/2010 11:24 */
+/* * * * * * * * * * * */
+
+#include "basicIncludes.h"
+
+#define combinedGraphics_cxx
+#include "combinedGraphics.C"
+
+#ifndef COMBINEDGRAPHICSCONTROL_H
+#define COMBINEDGRAPHICSCONTROL_H
+
+class combinedGraphicsControl : public utilities
+{
+	public:
+		combinedGraphics* m_combinedGraphics;
+		TFile* m_histfile;
+		
+	
+	public:
+		combinedGraphicsControl();
+		~combinedGraphicsControl();
+		void initialize(string sAnalysisSelector, string muonSelector);
+		void finalize();
+		
+		void execute();
+		void executeAfb();
+		void executeTrig();
+		void executeTrigTRUvsTnP();
+		void executeTrigMCvsData();
+		void executeTrigDataMap();
+
+	private:
+};
+#endif
+

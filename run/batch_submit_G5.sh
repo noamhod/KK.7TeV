5e9688ee3268ecf0195213596394a1df2cec7db0
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2011-08-01 20:46:43 +0000

    remove old restored

--- run/batch_submit_G5.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash/
-
-#G5
- source panda_run.sh   data   00165821   group10.phys-sm.data10_7TeV.00165821.physics_Muons.recon.ESD.f293.WZphys.100612.01.D3PD/
- source panda_run.sh   data   00165954   group10.phys-sm.data10_7TeV.00165954.physics_Muons.recon.ESD.f294.WZphys.100612.01.D3PD/
- source panda_run.sh   data   00165956   group10.phys-sm.data10_7TeV.00165956.physics_Muons.recon.ESD.f294.WZphys.100612.01.D3PD/
- source panda_run.sh   data   00166097   group10.phys-sm.data10_7TeV.00166097.physics_Muons.recon.ESD.f294.WZphys.100612.01.D3PD/
- source panda_run.sh   data   00166142   group10.phys-sm.data10_7TeV.00166142.physics_Muons.recon.ESD.f294.WZphys.100612.01.D3PD/
- source panda_run.sh   data   00166143   group10.phys-sm.data10_7TeV.00166143.physics_Muons.recon.ESD.f294.WZphys.100612.01.D3PD/

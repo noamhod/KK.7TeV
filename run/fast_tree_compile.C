0ec30a15162bccf1ce21751302ef3c55843a9e86
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2011-08-29 14:33:38 +0000

    t302 is back, updated the FB false identify analysis

--- run/fast_tree_compile.C
+++ run/fast_tree_compile.C
@@ -4,6 +4,6 @@
 	gROOT->ProcessLine(".include ./");
 	
 	gROOT->ProcessLine(".L fast_tree_run.C++");
-	gROOT->ProcessLine("setisMC(false)");
+	gROOT->ProcessLine("setisMC(true)");
 	gROOT->ProcessLine("run()");
 }

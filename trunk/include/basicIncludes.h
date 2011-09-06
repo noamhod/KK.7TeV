6c670d844e4e8797b2a9016759ad1c3fa7fdaafa
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2011-09-06 22:23:53 +0000

    added ellipticity analysis

--- trunk/include/basicIncludes.h
+++ trunk/include/basicIncludes.h
@@ -195,6 +195,13 @@ enum pdtEnum { // *** note that this is not a complete list of pdt's particles *
 	PDTFIRSTBARYON=1103, PDTLASTBARYON=5554
 };
 
+enum models
+{
+	ZPd3pd=-2,
+	Z0d3pd=-1,
+	Z0,ZP,KK,
+	DT
+};
 
 #endif
 

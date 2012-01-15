2b53977b6021a5171bb499279bda7fa3f2f03b89
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2012-01-15 01:25:45 +0000

    updated hmap2d with PDFs

--- ruby/hstack.rb
+++ ruby/hstack.rb
@@ -10,21 +10,23 @@ root = "$ROOTSYS/bin/root.exe -b -l -q"
 
 inlist =
 [
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=Nvxp_no_puwgt   --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=Nvxp_with_puwgt --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=Mass            --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=MassNumbers     --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=Mass_limit_     --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=pTLeading       --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=pTSubleading    --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=EtaLeading      --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=EtaSubleading   --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=PhiLeading      --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=PhiSubleading   --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=EtaQ            --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=QT              --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=yQ              --istru=0",
-	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=Isolation       --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=Nvxp_no_puwgt    --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=Nvxp_with_puwgt  --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=Mass             --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=MassNumbers      --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=Mass_limit_      --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=pTLeading        --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=pTSubleading     --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=EtaLeading       --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=EtaSubleading    --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=PhiLeading       --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=PhiSubleading    --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=EtaQ             --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=QT               --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=yQ               --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=IsolationFull    --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=IsolationLow     --istru=0",
+	"#{root} hstack.C+ --ftype=noPUrw_noEWkFsig_noCoupScale --htype=MassAntiIsolated --istru=0",
 ]
 outlist = []
 

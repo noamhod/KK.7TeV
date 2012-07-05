484554410fdd5c73e132297deec7ab8c5af8755a
noam.com <noam.com@1671584c-62dd-a2a1-72dd-838a7ae91a60>
2012-07-05 21:37:57 +0000

    fixed abug in the id fetching, added missing files

---
+++ trunk/pythia8/theory_analysis_compile.C
@@ -0,0 +1,49 @@
+{
+	//// usage: //////////////////////////////////////////////////////////////////////////////////////
+	//// broot -b -l -q theory_analysis_compile.C --mcname=KK --channel=mumu --mBSM=1000 --runNum=1 /////
+	//// mcname  = DY,ZP_ssm,ZP_psi,ZP_chi,ZP_eta,KK /////////////////////////////////////////////////
+	//// channel = mumu,ee ///////////////////////////////////////////////////////////////////////////
+	//// mBSM    = -1(for DY), >0 else ///////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+
+	gROOT->ProcessLine(".include ../include/");
+	gROOT->ProcessLine(".include ../src/");
+	gROOT->ProcessLine(".include ./");
+	
+	TString mcname  = "";
+	TString channel = "";
+	TString massbsm = "";
+	TString runnumb = "";
+
+	for(int i=0 ; i<gApplication->Argc() ; i++)
+	{
+		printf("Arg  %d:  %s\n",i,gApplication->Argv(i));
+	}
+	if(gApplication->Argc()>13 || gApplication->Argc()<5)
+	{
+		cout << "gApplication->Argc()=" << gApplication->Argc() << endl;
+		exit(-1);
+	}
+	
+	TString arg1 = gApplication->Argv(1);
+	for(int i=0 ; i<gApplication->Argc() ; i++)
+	{
+		TString arg = gApplication->Argv(i);
+		if(arg.Contains("--mcname="))  mcname  = arg.ReplaceAll("--mcname=","");
+		if(arg.Contains("--channel=")) channel = arg.ReplaceAll("--channel=","");
+		if(arg.Contains("--mBSM="))    massbsm = arg.ReplaceAll("--mBSM=","");
+		if(arg.Contains("--runNum="))  runnumb = arg.ReplaceAll("--runNum=","");
+	}
+	
+	cout << "running with mcname  = " << mcname << endl;
+	cout << "running with channel = " << channel << endl;
+	cout << "running with mBSM    = " << massbsm << endl;
+	cout << "running with runnumb = " << runnumb << endl;
+
+	TString sLine = "\""+mcname+"\",\""+channel+"\","+massbsm+","+runnumb;
+	cout << "Arguments: " << sLine << endl;
+	
+	gROOT->ProcessLine(".L theory_analysis.C++");
+	cout << "HERE " << endl;
+	gROOT->ProcessLine("analysis("+sLine+")");
+}
